973b7678559f48f130f82317abbb2271c109d989
Hydraheads <Hydraheads42@gmail.com>
2023-08-30 19:53:51 -0500

    Initial commit

---
+++ src/main/java/net/hydra/jojomod/RoundaboutModClient.java
@@ -0,0 +1,10 @@
+package net.hydra.jojomod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class RoundaboutModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
