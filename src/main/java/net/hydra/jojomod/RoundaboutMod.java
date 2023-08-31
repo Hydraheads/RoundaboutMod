eacd67f82c14530ab3ff215c0c782aa119e82581
Hydraheads <Hydraheads42@gmail.com>
2023-08-31 10:25:47 -0500

    Discord Test

--- src/main/java/net/hydra/jojomod/RoundaboutMod.java
+++ src/main/java/net/hydra/jojomod/RoundaboutMod.java
@@ -16,7 +16,6 @@ public class RoundaboutMod implements ModInitializer {
 	public void onInitialize() {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
 		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
 		ModItems.registerModItems();
 		ModItemGroups.registerItemGroups();
 		LOGGER.info("Hello Fabric world!");
