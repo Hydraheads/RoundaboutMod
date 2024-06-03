eb5149f855710e8c7ff0165c1f8ed48015799bfb
Hydraheads <Hydraheads42@gmail.com>
2024-06-03 13:13:51 -0500

    TS works on rain, clouds, and snow

--- common/src/main/java/net/hydra/jojomod/mixin/ZWorldRenderer.java
+++ common/src/main/java/net/hydra/jojomod/mixin/ZWorldRenderer.java
@@ -17,8 +17,10 @@ import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyArgs;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.invoke.arg.Args;
 
 import java.sql.Time;
@@ -69,4 +71,13 @@ public class ZWorldRenderer {
         return $$1;
     }
 
+    @Inject(method = "tick", at = @At(value = "HEAD"), cancellable = true)
+    private void RoundaboutCancelRenderTicks(CallbackInfo ci){
+        LivingEntity player = Minecraft.getInstance().player;
+        if (player != null) {
+            if (((TimeStop) player.level()).inTimeStopRange(player)) {
+                ci.cancel();
+            }
+        }
+    }
 }
