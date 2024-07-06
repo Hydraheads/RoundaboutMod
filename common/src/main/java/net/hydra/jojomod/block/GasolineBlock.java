07259aa529924919f67870d48ef54f8d5ab4254a
Hydraheads <Hydraheads42@gmail.com>
2024-07-06 00:20:50 -0500

    gasoline work

--- common/src/main/java/net/hydra/jojomod/block/GasolineBlock.java
+++ common/src/main/java/net/hydra/jojomod/block/GasolineBlock.java
@@ -9,19 +9,33 @@ import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.StateDefinition;
+import net.minecraft.world.level.block.state.properties.BlockStateProperties;
+import net.minecraft.world.level.block.state.properties.BooleanProperty;
+import net.minecraft.world.level.block.state.properties.IntegerProperty;
 import net.minecraft.world.phys.shapes.CollisionContext;
 import net.minecraft.world.phys.shapes.VoxelShape;
 
 public class GasolineBlock extends Block {
+        public static final IntegerProperty LEVEL = BlockStateProperties.LEVEL;
         protected static final VoxelShape SHAPE = Block.box(0.0, 0.001, 0.0, 16.0, 0.1, 16.0);
 
         public GasolineBlock(BlockBehaviour.Properties $$0) {
             super($$0);
+            this.registerDefaultState(this.stateDefinition.any().setValue(LEVEL, Integer.valueOf(0)));
         }
-        @SuppressWarnings("deprecation")
+        public GasolineBlock(BlockBehaviour.Properties $$0, int stage) {
+            super($$0);
+            this.registerDefaultState(this.stateDefinition.any().setValue(LEVEL, Integer.valueOf(stage)));
+        }
+
         @Override
         public VoxelShape getShape(BlockState $$0, BlockGetter $$1, BlockPos $$2, CollisionContext $$3) {
             return SHAPE;
         }
 
+    @Override
+    protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> $$0) {
+        $$0.add(LEVEL);
+    }
 }
--- fabric/src/main/java/net/hydra/jojomod/client/RoundaboutFabricClient.java
+++ fabric/src/main/java/net/hydra/jojomod/client/RoundaboutFabricClient.java
@@ -1,17 +1,22 @@
 package net.hydra.jojomod.client;
 
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
+import net.hydra.jojomod.block.ModBlocks;
 import net.hydra.jojomod.networking.FabricPacketManager;
 import net.hydra.jojomod.networking.FabricPackets;
 import net.hydra.jojomod.particles.ModParticlesClient;
 import net.hydra.jojomod.registry.FabricEntities;
 import net.hydra.jojomod.registry.FabricEntityClient;
 import net.hydra.jojomod.registry.FabricKeyInputs;
+import net.minecraft.client.renderer.RenderType;
+import net.minecraft.client.renderer.entity.layers.RenderLayer;
 
 public class RoundaboutFabricClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
         FabricKeyInputs.register();
+        BlockRenderLayerMap.INSTANCE.putBlocks(RenderType.cutout(), ModBlocks.GASOLINE_SPLATTER);
         FabricPacketManager.registerS2CPackets();
         ModParticlesClient.registerClientParticles();
         FabricEntityClient.register();
