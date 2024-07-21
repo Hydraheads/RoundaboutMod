8e9d1ce792ef106490da4988bb22046b305fe573
Hydraheads <Hydraheads42@gmail.com>
2024-07-21 08:27:17 -0500

    Loca Spawns naturally (fabric)

--- common/src/main/java/net/hydra/jojomod/block/ModFeatures.java
+++ common/src/main/java/net/hydra/jojomod/block/ModFeatures.java
@@ -22,30 +22,5 @@ import net.minecraft.world.level.levelgen.placement.InSquarePlacement;
 import net.minecraft.world.level.levelgen.placement.PlacedFeature;
 
 public class ModFeatures {
-
-    /**Configured Features*/
-    /**
-
-    public static final ResourceKey<ConfiguredFeature<?, ?>> NATURAL_LOCACACA = registerKey("natural_locacaca");
-    public static ResourceKey<ConfiguredFeature<?, ?>> registerKey(String name) {
-        return ResourceKey.create(Registries.CONFIGURED_FEATURE, new ResourceLocation(Roundabout.MOD_ID, name));
-    }
-    public static final ConfiguredFeature<?, ?> NATURAL_LOCACACA_CONFIG = new ConfiguredFeature<FeatureConfiguration, Feature> (Feature.FLOWER, new RandomPatchConfiguration(64, 6, 2, PlacementUtils.onlyWhenEmpty(Feature.SIMPLE_BLOCK, new SimpleBlockConfiguration(BlockStateProvider.simple(ModBlocks.LOCACACA_BLOCK)))));
-
-    public static void bootstrap(BootstapContext<ConfiguredFeature<?, ?>> bootstapContext) {
-        FeatureUtils.register(bootstapContext,NATURAL_LOCACACA,
-                Feature.FLOWER, new RandomPatchConfiguration(64, 6, 2, PlacementUtils.onlyWhenEmpty(Feature.SIMPLE_BLOCK, new SimpleBlockConfiguration(BlockStateProvider.simple(ModBlocks.LOCACACA_BLOCK)))));
-    }
-     */
-
-    /**Placed Features*/
-/**
-    public static final ResourceKey<PlacedFeature> PLACED_LOCACACA = PlacementUtils.createKey("placed_locacaca");
-
-    public static void bootstrapPlaced(BootstapContext<PlacedFeature> context) {
-        HolderGetter<ConfiguredFeature<?, ?>> holdergetter = context.lookup(Registries.CONFIGURED_FEATURE);
-        Holder<ConfiguredFeature<?, ?>> holder = holdergetter.getOrThrow(NATURAL_LOCACACA);
-        PlacementUtils.register(context, PLACED_LOCACACA, holder, CountPlacement.of(3), InSquarePlacement.spread(), PlacementUtils.HEIGHTMAP, BiomeFilter.biome());
-    }
- */
+    public static final ResourceLocation LOCACACA_PLACED = new ResourceLocation(Roundabout.MOD_ID, "placed_locacaca");
 }
--- fabric/src/main/java/net/hydra/jojomod/world/gen/ModWorldGeneration.java
+++ fabric/src/main/java/net/hydra/jojomod/world/gen/ModWorldGeneration.java
@@ -1,8 +1,21 @@
 package net.hydra.jojomod.world.gen;
 
+import net.fabricmc.fabric.api.biome.v1.BiomeModifications;
+import net.fabricmc.fabric.api.biome.v1.BiomeSelectors;
+import net.hydra.jojomod.block.ModFeatures;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.tags.BiomeTags;
+import net.minecraft.world.level.biome.Biome;
+import net.minecraft.world.level.biome.Biomes;
+import net.minecraft.world.level.levelgen.GenerationStep;
+
 public class ModWorldGeneration {
     /** @see WorldEntityGeneration*/
     public static void generateWorldGen(){
         WorldEntityGeneration.addSpawns();
+
+        BiomeModifications.addFeature(BiomeSelectors.tag(BiomeTags.IS_JUNGLE), GenerationStep.Decoration.VEGETAL_DECORATION,
+                ResourceKey.create(Registries.PLACED_FEATURE, ModFeatures.LOCACACA_PLACED));
     }
 }
