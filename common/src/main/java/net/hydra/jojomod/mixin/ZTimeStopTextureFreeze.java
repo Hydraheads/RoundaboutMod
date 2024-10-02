dc1646b8ee4f7e14e241393b55e72f9cd1424169
StandoByte <94539965+StandoByte@users.noreply.github.com>
2024-10-02 23:18:05 +0300

    Fixed an NPE with Optifine

--- common/src/main/java/net/hydra/jojomod/mixin/ZTimeStopTextureFreeze.java
+++ common/src/main/java/net/hydra/jojomod/mixin/ZTimeStopTextureFreeze.java
@@ -3,6 +3,7 @@ package net.hydra.jojomod.mixin;
 import net.hydra.jojomod.event.powers.TimeStop;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.texture.TextureManager;
+import net.minecraft.world.entity.Entity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -12,7 +13,8 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 public class ZTimeStopTextureFreeze {
     @Inject(method = "tick", at = @At("HEAD"), cancellable = true)
     public void roundaboutCancelTextures(CallbackInfo ci) {
-        if (((TimeStop) Minecraft.getInstance().player.level()).inTimeStopRange(Minecraft.getInstance().player)) {
+        Entity player = Minecraft.getInstance().player;
+        if (player != null && ((TimeStop) player.level()).inTimeStopRange(player)) {
             ci.cancel();
         }
     }
