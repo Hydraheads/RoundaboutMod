1d14335a169e5f600e7860ef06f440ae4579ec64
Hydraheads <Hydraheads42@gmail.com>
2024-11-10 01:27:57 -0600

    translation updates

--- common/src/main/java/net/hydra/jojomod/event/powers/stand/PowersJustice.java
+++ common/src/main/java/net/hydra/jojomod/event/powers/stand/PowersJustice.java
@@ -132,7 +132,7 @@ public class PowersJustice extends DashPreset {
     public void renderIcons(GuiGraphics context, int x, int y) {
 
         if (isHoldingSneak()){
-            setSkillIcon(context, x, y, 1, StandIcons.JUSTICE_DISGUISE, PowerIndex.NONE);
+            setSkillIcon(context, x, y, 1, StandIcons.JUSTICE_DISGUISE, PowerIndex.SKILL_1_SNEAK);
         } else {
             setSkillIcon(context, x, y, 1, StandIcons.JUSTICE_CAST_FOG, PowerIndex.NO_CD);
 
