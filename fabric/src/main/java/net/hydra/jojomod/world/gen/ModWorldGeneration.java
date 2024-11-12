0ab50c64f6b50d334a0d478796849a24acfbc7bc
cwse1 <73314023+cwse1@users.noreply.github.com>
2024-11-12 13:53:51 -0500

    Experimenting with meteors as placed features

--- fabric/src/main/java/net/hydra/jojomod/world/gen/ModWorldGeneration.java
+++ fabric/src/main/java/net/hydra/jojomod/world/gen/ModWorldGeneration.java
@@ -5,17 +5,22 @@ import net.fabricmc.fabric.api.biome.v1.BiomeSelectors;
 import net.hydra.jojomod.block.ModFeatures;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.resources.ResourceKey;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.tags.BiomeTags;
 import net.minecraft.world.level.biome.Biome;
 import net.minecraft.world.level.biome.Biomes;
 import net.minecraft.world.level.levelgen.GenerationStep;
 
 public class ModWorldGeneration {
-    /** @see WorldEntityGeneration*/
-    public static void generateWorldGen(){
+    /**
+     * @see WorldEntityGeneration
+     */
+    public static void generateWorldGen() {
         WorldEntityGeneration.addSpawns();
 
         BiomeModifications.addFeature(BiomeSelectors.tag(BiomeTags.IS_JUNGLE), GenerationStep.Decoration.VEGETAL_DECORATION,
                 ResourceKey.create(Registries.PLACED_FEATURE, ModFeatures.LOCACACA_PLACED));
+        BiomeModifications.addFeature(BiomeSelectors.foundInOverworld(), GenerationStep.Decoration.SURFACE_STRUCTURES,
+                ResourceKey.create(Registries.PLACED_FEATURE, ModFeatures.METEORITE_PLACED));
     }
 }
