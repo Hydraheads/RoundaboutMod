8214552b957e28c24a830986d6269eb61f7fb287
Hydraheads <Hydraheads42@gmail.com>
2025-01-09 18:21:53 -0600

    Elytra emoting

--- common/src/main/java/net/hydra/jojomod/mixin/ZElytraModel.java
+++ common/src/main/java/net/hydra/jojomod/mixin/ZElytraModel.java
@@ -1,16 +1,13 @@
 package net.hydra.jojomod.mixin;
 
 import net.hydra.jojomod.access.IPlayerEntity;
-import net.hydra.jojomod.access.IPlayerModel;
 import net.hydra.jojomod.event.index.Poses;
 import net.minecraft.client.animation.AnimationChannel;
 import net.minecraft.client.animation.AnimationDefinition;
 import net.minecraft.client.animation.Keyframe;
-import net.minecraft.client.animation.KeyframeAnimations;
-import net.minecraft.client.model.HumanoidModel;
-import net.minecraft.client.model.PlayerModel;
+import net.minecraft.client.model.AgeableListModel;
+import net.minecraft.client.model.ElytraModel;
 import net.minecraft.client.model.geom.ModelPart;
-import net.minecraft.client.model.geom.builders.PartDefinition;
 import net.minecraft.util.Mth;
 import net.minecraft.world.entity.AnimationState;
 import net.minecraft.world.entity.LivingEntity;
@@ -29,55 +26,25 @@ import java.util.Map;
 import java.util.Objects;
 import java.util.Optional;
 
-@Mixin(PlayerModel.class)
-public abstract class ZPlayerModel<T extends LivingEntity> extends HumanoidModel<T> implements IPlayerModel {
+@Mixin(ElytraModel.class)
+public abstract class ZElytraModel<T extends LivingEntity> extends AgeableListModel<T> {
 
     @Shadow
     @Final
-    private List<ModelPart> parts;
+    private ModelPart rightWing;
     @Shadow
     @Final
-    private boolean slim;
+    private ModelPart leftWing;
 
-    @Shadow @Final private ModelPart cloak;
-
-    public ZPlayerModel(ModelPart $$0) {
-        super($$0);
-    }
-
-    @Override
-    @Unique
-    public boolean roundabout$getSlim(){
-        return this.slim;
-    }
-
-    PartDefinition roundabout$partDef;
-    @Unique
-    private static final Vector3f roundabout$ANIMATION_VECTOR_CACHE = new Vector3f();
     @Inject(method = "setupAnim(Lnet/minecraft/world/entity/LivingEntity;FFFFF)V", at = @At(value = "HEAD"))
     public void roundabout$SetupAnim4(T $$0, float $$1, float $$2, float $$3, float $$4, float $$5, CallbackInfo ci) {
-        this.head.resetPose();
-        this.body.resetPose();
-        this.rightLeg.resetPose();
-        this.leftLeg.resetPose();
-        this.rightArm.resetPose();
-        this.leftArm.resetPose();
-        this.cloak.resetPose();
+        this.leftWing.resetPose();
+        this.rightWing.resetPose();
     }
-    @Inject(method = "setupAnim(Lnet/minecraft/world/entity/LivingEntity;FFFFF)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/model/geom/ModelPart;copyFrom(Lnet/minecraft/client/model/geom/ModelPart;)V", shift = At.Shift.BEFORE, ordinal = 0))
-    public void roundabout$SetupAnim2(T $$0, float $$1, float $$2, float $$3, float $$4, float $$5, CallbackInfo ci) {
-
+    @Inject(method = "setupAnim(Lnet/minecraft/world/entity/LivingEntity;FFFFF)V", at = @At(value = "TAIL"))
+    public void roundabout$SetupAnim5(T $$0, float $$1, float $$2, float $$3, float $$4, float $$5, CallbackInfo ci) {
         if ($$0 instanceof Player) {
             IPlayerEntity ipe = ((IPlayerEntity) $$0);
-            if (ipe.roundabout$GetPoseEmote() != Poses.NONE.id) {
-                this.head.resetPose();
-                this.body.resetPose();
-                this.rightLeg.resetPose();
-                this.leftLeg.resetPose();
-                this.rightArm.resetPose();
-                this.leftArm.resetPose();
-            }
-
             this.roundabout$animate(ipe.getWry(), Poses.WRY.ad, $$3, 1f);
             this.roundabout$animate(ipe.getGiorno(), Poses.GIORNO.ad, $$3, 1f);
             this.roundabout$animate(ipe.getJoseph(), Poses.JOSEPH.ad, $$3, 1f);
@@ -87,37 +54,23 @@ public abstract class ZPlayerModel<T extends LivingEntity> extends HumanoidModel
             this.roundabout$animate(ipe.getWamuu(), Poses.WAMUU.ad, $$3, 1f);
             this.roundabout$animate(ipe.getJotaro(), Poses.JOTARO.ad, $$3, 1f);
             this.roundabout$animate(ipe.getJonathan(), Poses.JONATHAN.ad, $$3, 1f);
-            this.hat.copyFrom(this.head);
         }
     }
-    @Inject(method = "setupAnim(Lnet/minecraft/world/entity/LivingEntity;FFFFF)V", at = @At(value = "TAIL"))
-    public void roundabout$SetupAnim3(T $$0, float $$1, float $$2, float $$3, float $$4, float $$5, CallbackInfo ci) {
-        if ($$0 instanceof Player) {
-            IPlayerEntity ipe = ((IPlayerEntity) $$0);
-            this.roundabout$animate2(ipe.getWry(), Poses.WRY.ad, $$3, 1f);
-            this.roundabout$animate2(ipe.getGiorno(), Poses.GIORNO.ad, $$3, 1f);
-            this.roundabout$animate2(ipe.getJoseph(), Poses.JOSEPH.ad, $$3, 1f);
-            this.roundabout$animate2(ipe.getKoichi(), Poses.KOICHI.ad, $$3, 1f);
-            this.roundabout$animate2(ipe.getOhNo(), Poses.OH_NO.ad, $$3, 1f);
-            this.roundabout$animate2(ipe.getTortureDance(), Poses.TORTURE_DANCE.ad, $$3, 1f);
-            this.roundabout$animate2(ipe.getWamuu(), Poses.WAMUU.ad, $$3, 1f);
-            this.roundabout$animate2(ipe.getJotaro(), Poses.JOTARO.ad, $$3, 1f);
-            this.roundabout$animate2(ipe.getJonathan(), Poses.JONATHAN.ad, $$3, 1f);
-        }
-    }
+
+    @Unique
+    private static final Vector3f roundabout$ANIMATION_VECTOR_CACHE = new Vector3f();
     @Unique
     protected void roundabout$animate(AnimationState $$0, AnimationDefinition $$1, float $$2, float $$3) {
         $$0.updateTime($$2, $$3);
         $$0.ifStarted($$1x -> roundabout$animate($$1, $$1x.getAccumulatedTime(), 1.0F,
                 roundabout$ANIMATION_VECTOR_CACHE));
     }
-
     @Unique
     public void roundabout$animate(AnimationDefinition p_232321_, long p_232322_, float p_232323_, Vector3f p_253861_) {
         float f = roundabout$getElapsedSeconds(p_232321_, p_232322_);
 
         for(Map.Entry<String, List<AnimationChannel>> entry : p_232321_.boneAnimations().entrySet()) {
-            Optional<ModelPart> optional = roundabout$getAnyDescendantWithName(entry.getKey());
+            Optional<ModelPart> optional = roundabout$getRight(entry.getKey());
             List<AnimationChannel> list = entry.getValue();
             optional.ifPresent((p_232330_) -> {
                 list.forEach((p_288241_) -> {
@@ -141,22 +94,8 @@ public abstract class ZPlayerModel<T extends LivingEntity> extends HumanoidModel
                 });
             });
         }
-
-    }
-
-    @Unique
-    protected void roundabout$animate2(AnimationState $$0, AnimationDefinition $$1, float $$2, float $$3) {
-        $$0.updateTime($$2, $$3);
-        $$0.ifStarted($$1x -> roundabout$animate2($$1, $$1x.getAccumulatedTime(), 1.0F,
-                roundabout$ANIMATION_VECTOR_CACHE));
-    }
-
-    @Unique
-    public void roundabout$animate2(AnimationDefinition p_232321_, long p_232322_, float p_232323_, Vector3f p_253861_) {
-        float f = roundabout$getElapsedSeconds(p_232321_, p_232322_);
-
         for(Map.Entry<String, List<AnimationChannel>> entry : p_232321_.boneAnimations().entrySet()) {
-            Optional<ModelPart> optional = roundabout$getAnyDescendantWithName2(entry.getKey());
+            Optional<ModelPart> optional = roundabout$getLeft(entry.getKey());
             List<AnimationChannel> list = entry.getValue();
             optional.ifPresent((p_232330_) -> {
                 list.forEach((p_288241_) -> {
@@ -180,37 +119,24 @@ public abstract class ZPlayerModel<T extends LivingEntity> extends HumanoidModel
                 });
             });
         }
-
     }
     @Unique
     private static float roundabout$getElapsedSeconds(AnimationDefinition p_232317_, long p_232318_) {
         float f = (float)p_232318_ / 1000.0F;
         return p_232317_.looping() ? f % p_232317_.lengthInSeconds() : f;
     }
-
     @Unique
-    public Optional<ModelPart> roundabout$getAnyDescendantWithName(String $$0) {
+    public Optional<ModelPart> roundabout$getRight(String $$0) {
         if (Objects.equals($$0, "body")){
-            return Optional.of(this.body);
-        } else if (Objects.equals($$0, "head")){
-            return Optional.of(this.head);
-        } else if (Objects.equals($$0, "right_leg")){
-            return Optional.of(this.rightLeg);
-        } else if (Objects.equals($$0, "left_leg")){
-            return Optional.of(this.leftLeg);
-        } else if (Objects.equals($$0, "right_arm")){
-            return Optional.of(this.rightArm);
-        } else if (Objects.equals($$0, "left_arm")){
-            return Optional.of(this.leftArm);
+            return Optional.of(this.rightWing);
         }
         return Optional.empty();
     }
     @Unique
-    public Optional<ModelPart> roundabout$getAnyDescendantWithName2(String $$0) {
+    public Optional<ModelPart> roundabout$getLeft(String $$0) {
         if (Objects.equals($$0, "body")){
-            return Optional.of(this.cloak);
+            return Optional.of(this.leftWing);
         }
         return Optional.empty();
     }
-
 }
