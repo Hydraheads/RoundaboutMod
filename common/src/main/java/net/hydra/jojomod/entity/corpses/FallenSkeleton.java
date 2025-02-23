caa51d284e6967a8166be82f6d7dfaf941d0e39d
Hydraheads <Hydraheads42@gmail.com>
2025-02-22 20:38:25 -0600

    sKELETONS

--- common/src/main/java/net/hydra/jojomod/entity/corpses/FallenSkeleton.java
+++ common/src/main/java/net/hydra/jojomod/entity/corpses/FallenSkeleton.java
@@ -7,7 +7,14 @@ import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.entity.*;
 import net.minecraft.world.entity.ai.attributes.AttributeSupplier;
 import net.minecraft.world.entity.ai.attributes.Attributes;
+import net.minecraft.world.entity.ai.goal.RangedBowAttackGoal;
+import net.minecraft.world.entity.monster.AbstractSkeleton;
 import net.minecraft.world.entity.monster.RangedAttackMob;
+import net.minecraft.world.entity.projectile.AbstractArrow;
+import net.minecraft.world.entity.projectile.ProjectileUtil;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.item.ProjectileWeaponItem;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.state.BlockState;
 
@@ -17,17 +24,26 @@ public class FallenSkeleton extends FallenMob implements RangedAttackMob  {
     }
 
     public static AttributeSupplier.Builder createAttributes() {
-        return Mob.createMobAttributes().add(Attributes.MOVEMENT_SPEED, 0.5D).add(Attributes.MAX_HEALTH, 20)
+        return Mob.createMobAttributes().add(Attributes.MOVEMENT_SPEED, 0.25).add(Attributes.MAX_HEALTH, 20)
                 .add(Attributes.ATTACK_DAMAGE, 1).
                 add(Attributes.FOLLOW_RANGE, 48.0D);
     }
-
+    public void setActivated(boolean bool){
+        if (bool){
+            this.setItemSlot(EquipmentSlot.MAINHAND, new ItemStack(Items.BOW));
+        } else {
+            this.setItemSlot(EquipmentSlot.MAINHAND, ItemStack.EMPTY);
+        }
+        super.setActivated(bool);
+    }
+    @Override
+    protected void registerGoals() {
+        this.goalSelector.addGoal(4, new FallenRangedAttackGoal<>(this, 1.0, 20, 15.0F));
+        this.addBehaviourGoals();
+    }
     @Override
     public String getData(){
         return "skeleton";
-    }
-    @Override
-    public void performRangedAttack(LivingEntity var1, float var2) {
     }
 
 
@@ -70,6 +86,29 @@ public class FallenSkeleton extends FallenMob implements RangedAttackMob  {
     @Override
     public MobType getMobType() {
         return MobType.UNDEAD;
+    }
+
+
+    @Override
+    public void performRangedAttack(LivingEntity $$0, float $$1) {
+        ItemStack $$2 = this.getProjectile(this.getItemInHand(ProjectileUtil.getWeaponHoldingHand(this, Items.BOW)));
+        AbstractArrow $$3 = this.getArrow($$2, $$1);
+        double $$4 = $$0.getX() - this.getX();
+        double $$5 = $$0.getY(0.3333333333333333) - $$3.getY();
+        double $$6 = $$0.getZ() - this.getZ();
+        double $$7 = Math.sqrt($$4 * $$4 + $$6 * $$6);
+        $$3.shoot($$4, $$5 + $$7 * 0.2F, $$6, 1.6F, (float)(14 - this.level().getDifficulty().getId() * 4));
+        this.playSound(SoundEvents.SKELETON_SHOOT, 1.0F, 1.0F / (this.getRandom().nextFloat() * 0.4F + 0.8F));
+        this.level().addFreshEntity($$3);
+    }
+
+    protected AbstractArrow getArrow(ItemStack $$0, float $$1) {
+        return ProjectileUtil.getMobArrow(this, $$0, $$1);
+    }
+
+    @Override
+    public boolean canFireProjectileWeapon(ProjectileWeaponItem $$0) {
+        return $$0 == Items.BOW;
     }
 }
 
