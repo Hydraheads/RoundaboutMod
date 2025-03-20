b9c85b9e8f45d882881faed772f813cb27db9462
Hydraheads <Hydraheads42@gmail.com>
2025-03-20 10:45:50 -0500

    executioner axe bug fix

--- common/src/main/java/net/hydra/jojomod/mixin/StandUserEntity.java
+++ common/src/main/java/net/hydra/jojomod/mixin/StandUserEntity.java
@@ -1904,7 +1904,7 @@ public abstract class StandUserEntity extends Entity implements StandUser {
         if ($$0.getDirectEntity() != null) {
             if (($$0.getDirectEntity() instanceof Player PE && (PE.getMainHandItem().is(ModItems.EXECUTIONER_AXE)
             || (PE.getMainHandItem().is(ModItems.SCISSORS) && ((StandUser)PE).roundabout$getStandPowers()
-            instanceof PowersJustice))) && !$$0.is(ModDamageTypes.CORPSE)) {
+            instanceof PowersJustice))) && $$0.is(DamageTypes.PLAYER_ATTACK)) {
                 LivingEntity ths = ((LivingEntity)(Object)this);
                 boolean marked = false;
                 FallenMob mb = null;
