fd5abc0e01e9bff30334b0e1c9bea8de34d2a30c
Hydraheads <Hydraheads42@gmail.com>
2025-04-04 22:09:14 -0500

    Fixed all stand fire dmg triggering gas explosions

--- common/src/main/java/net/hydra/jojomod/mixin/StandUserEntity.java
+++ common/src/main/java/net/hydra/jojomod/mixin/StandUserEntity.java
@@ -1822,20 +1822,20 @@ public abstract class StandUserEntity extends Entity implements StandUser {
 
                 ci.setReturnValue(true);
                 return;
-            }
-        } else if ($$0.is(ModDamageTypes.STAND_FIRE)) {
-            float power = MainUtil.gasDamageMultiplier()*10;
-            this.roundabout$setGasolineTime(-1);
-            if (!this.level().isClientSide) {
-                ((ServerLevel) this.level()).sendParticles(ParticleTypes.FLAME, this.getX(), this.getY()+this.getEyeHeight(), this.getZ(),
-                        40, 0.0, 0.2, 0.0, 0.2);
-                ((ServerLevel) this.level()).sendParticles(ParticleTypes.EXPLOSION, this.getX(), this.getY()+this.getEyeHeight(), this.getZ(),
-                        1, 0.5, 0.5, 0.5, 0.2);
-                MainUtil.gasExplode(null, (ServerLevel) this.level(), this.getOnPos(), 0, 2, 4, power);
-            }
+            } else if ($$0.is(ModDamageTypes.STAND_FIRE)) {
+                float power = MainUtil.gasDamageMultiplier()*10;
+                this.roundabout$setGasolineTime(-1);
+                if (!this.level().isClientSide) {
+                    ((ServerLevel) this.level()).sendParticles(ParticleTypes.FLAME, this.getX(), this.getY()+this.getEyeHeight(), this.getZ(),
+                            40, 0.0, 0.2, 0.0, 0.2);
+                    ((ServerLevel) this.level()).sendParticles(ParticleTypes.EXPLOSION, this.getX(), this.getY()+this.getEyeHeight(), this.getZ(),
+                            1, 0.5, 0.5, 0.5, 0.2);
+                    MainUtil.gasExplode(null, (ServerLevel) this.level(), this.getOnPos(), 0, 2, 4, power);
+                }
 
-            ci.setReturnValue(true);
-            return;
+                ci.setReturnValue(true);
+                return;
+            }
         }
         if ($$0.getDirectEntity() != null && !$$0.is(ModDamageTypes.STAND_FIRE)) {
            if (this.roundabout$getStandPowers().preCanInterruptPower($$0.getDirectEntity(),MainUtil.isStandDamage($$0))) {
