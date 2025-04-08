72e7c38880c128d45f4f4d840ae157eb56158f57
Hydraheads <Hydraheads42@gmail.com>
2025-04-08 11:51:42 -0500

    fire geckolib compat fix

---
+++ common/src/main/java/net/hydra/jojomod/mixin/GeckolibCompatAttempt.java
@@ -0,0 +1,37 @@
+package net.hydra.jojomod.mixin;
+
+import net.hydra.jojomod.Roundabout;
+import net.minecraft.client.renderer.texture.SimpleTexture;
+import net.minecraft.client.renderer.texture.TextureManager;
+import org.spongepowered.asm.mixin.Mixin;
+import net.minecraft.client.renderer.texture.AbstractTexture;
+import net.minecraft.resources.ResourceLocation;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import java.util.Map;
+
+@Mixin(value = TextureManager.class, priority = 100)
+public abstract class GeckolibCompatAttempt {
+
+    //Geckolib destroys vanilla rendering functions so this mixin asserts the vanilla method if the namespace is roundabout
+    @Shadow @Final private Map<ResourceLocation, AbstractTexture> byPath;
+
+    @Shadow public abstract void register(ResourceLocation resourceLocation, AbstractTexture abstractTexture);
+
+    @Inject(method = "getTexture(Lnet/minecraft/resources/ResourceLocation;)Lnet/minecraft/client/renderer/texture/AbstractTexture;", at = @At("HEAD"), cancellable = true)
+    private void wrapAnimatableTexture(ResourceLocation path, CallbackInfoReturnable<AbstractTexture> callback) {
+        if (path.getNamespace().contains(Roundabout.MOD_ID)){
+            AbstractTexture $$1 = (AbstractTexture)this.byPath.get(path);
+            if ($$1 == null) {
+                $$1 = new SimpleTexture(path);
+                this.register((ResourceLocation)path, (AbstractTexture)$$1);
+            }
+
+            callback.setReturnValue((AbstractTexture)$$1);
+        }
+    }
+}
