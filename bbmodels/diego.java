e1b02506a17943da398b5acccf0fa8f5f65d8df4
Hydraheads <Hydraheads42@gmail.com>
2025-04-21 14:43:42 -0500

    Diego Visage + Parallel Diego jojo npc

---
+++ bbmodels/diego.java
@@ -0,0 +1,128 @@
+// Made with Blockbench 4.12.4
+// Exported for Minecraft version 1.17 or later with Mojang mappings
+// Paste this class into your mod and generate all required imports
+
+
+public class diego<T extends Entity> extends EntityModel<T> {
+	// This layer location should be baked with EntityRendererProvider.Context in the entity renderer and passed into this model's constructor
+	public static final ModelLayerLocation LAYER_LOCATION = new ModelLayerLocation(new ResourceLocation("modid", "diego"), "main");
+	private final ModelPart playerlike;
+	private final ModelPart full_body;
+	private final ModelPart head_part;
+	private final ModelPart head;
+	private final ModelPart hat;
+	private final ModelPart body_part;
+	private final ModelPart legs;
+	private final ModelPart right_legs;
+	private final ModelPart right_pants;
+	private final ModelPart right_leg;
+	private final ModelPart left_legs;
+	private final ModelPart left_leg;
+	private final ModelPart left_pants;
+	private final ModelPart upper_body;
+	private final ModelPart right_arms;
+	private final ModelPart right_arm;
+	private final ModelPart right_sleeve;
+	private final ModelPart left_arms;
+	private final ModelPart left_arm;
+	private final ModelPart left_sleeve;
+	private final ModelPart body;
+	private final ModelPart jacket;
+	private final ModelPart cloak;
+
+	public diego(ModelPart root) {
+		this.playerlike = root.getChild("playerlike");
+		this.full_body = this.playerlike.getChild("full_body");
+		this.head_part = this.full_body.getChild("head_part");
+		this.head = this.head_part.getChild("head");
+		this.hat = this.head_part.getChild("hat");
+		this.body_part = this.full_body.getChild("body_part");
+		this.legs = this.body_part.getChild("legs");
+		this.right_legs = this.legs.getChild("right_legs");
+		this.right_pants = this.right_legs.getChild("right_pants");
+		this.right_leg = this.right_legs.getChild("right_leg");
+		this.left_legs = this.legs.getChild("left_legs");
+		this.left_leg = this.left_legs.getChild("left_leg");
+		this.left_pants = this.left_legs.getChild("left_pants");
+		this.upper_body = this.body_part.getChild("upper_body");
+		this.right_arms = this.upper_body.getChild("right_arms");
+		this.right_arm = this.right_arms.getChild("right_arm");
+		this.right_sleeve = this.right_arms.getChild("right_sleeve");
+		this.left_arms = this.upper_body.getChild("left_arms");
+		this.left_arm = this.left_arms.getChild("left_arm");
+		this.left_sleeve = this.left_arms.getChild("left_sleeve");
+		this.body = this.upper_body.getChild("body");
+		this.jacket = this.upper_body.getChild("jacket");
+		this.cloak = this.upper_body.getChild("cloak");
+	}
+
+	public static LayerDefinition createBodyLayer() {
+		MeshDefinition meshdefinition = new MeshDefinition();
+		PartDefinition partdefinition = meshdefinition.getRoot();
+
+		PartDefinition playerlike = partdefinition.addOrReplaceChild("playerlike", CubeListBuilder.create(), PartPose.offset(0.0F, 24.0F, 0.0F));
+
+		PartDefinition full_body = playerlike.addOrReplaceChild("full_body", CubeListBuilder.create(), PartPose.offset(0.0F, 0.0F, 0.0F));
+
+		PartDefinition head_part = full_body.addOrReplaceChild("head_part", CubeListBuilder.create(), PartPose.offset(0.0F, -24.0F, 0.0F));
+
+		PartDefinition head = head_part.addOrReplaceChild("head", CubeListBuilder.create().texOffs(0, 0).addBox(-4.0F, -8.0F, -4.0F, 8.0F, 8.0F, 8.0F, new CubeDeformation(0.0F)), PartPose.offset(0.0F, 0.0F, 0.0F));
+
+		PartDefinition hat = head_part.addOrReplaceChild("hat", CubeListBuilder.create().texOffs(32, 0).addBox(-4.0F, -8.0F, -4.0F, 8.0F, 8.0F, 8.0F, new CubeDeformation(0.5F))
+		.texOffs(46, 64).addBox(-4.075F, -8.375F, -4.725F, 3.0F, 3.0F, 2.0F, new CubeDeformation(0.0F))
+		.texOffs(46, 74).addBox(1.1F, -8.375F, -4.725F, 3.0F, 3.0F, 2.0F, new CubeDeformation(0.0F))
+		.texOffs(46, 79).addBox(1.1F, -4.075F, -4.725F, 3.0F, 0.0F, 2.0F, new CubeDeformation(0.0F)), PartPose.offset(0.0F, 0.0F, 0.0F));
+
+		PartDefinition cube_r1 = hat.addOrReplaceChild("cube_r1", CubeListBuilder.create().texOffs(46, 69).addBox(-2.5F, -3.0F, -1.0F, 3.0F, 3.0F, 2.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(1.625F, -5.825F, -3.725F, 0.0F, 0.0F, -0.4276F));
+
+		PartDefinition cube_r2 = hat.addOrReplaceChild("cube_r2", CubeListBuilder.create().texOffs(44, 79).addBox(-4.0F, -3.0F, -2.0F, 10.0F, 1.0F, 3.0F, new CubeDeformation(-0.4F)), PartPose.offsetAndRotation(-0.975F, -2.5F, -4.0F, 0.4451F, 0.0F, 0.0F));
+
+		PartDefinition body_part = full_body.addOrReplaceChild("body_part", CubeListBuilder.create(), PartPose.offset(0.0F, 0.0F, 0.0F));
+
+		PartDefinition legs = body_part.addOrReplaceChild("legs", CubeListBuilder.create(), PartPose.offset(-5.0F, -24.0F, 0.0F));
+
+		PartDefinition right_legs = legs.addOrReplaceChild("right_legs", CubeListBuilder.create(), PartPose.offset(3.0F, 12.0F, 0.0F));
+
+		PartDefinition right_pants = right_legs.addOrReplaceChild("right_pants", CubeListBuilder.create().texOffs(0, 32).addBox(-1.9F, 0.0F, -2.0F, 4.0F, 12.0F, 4.0F, new CubeDeformation(0.25F)), PartPose.offset(-0.1F, 0.0F, 0.0F));
+
+		PartDefinition right_leg = right_legs.addOrReplaceChild("right_leg", CubeListBuilder.create().texOffs(0, 16).addBox(-2.0F, 0.0F, -2.0F, 4.0F, 12.0F, 4.0F, new CubeDeformation(0.0F)), PartPose.offset(0.0F, 0.0F, 0.0F));
+
+		PartDefinition left_legs = legs.addOrReplaceChild("left_legs", CubeListBuilder.create(), PartPose.offset(7.0F, 12.0F, 0.0F));
+
+		PartDefinition left_leg = left_legs.addOrReplaceChild("left_leg", CubeListBuilder.create().texOffs(16, 48).addBox(-2.0F, 0.0F, -2.0F, 4.0F, 12.0F, 4.0F, new CubeDeformation(0.0F)), PartPose.offset(0.0F, 0.0F, 0.0F));
+
+		PartDefinition left_pants = left_legs.addOrReplaceChild("left_pants", CubeListBuilder.create().texOffs(0, 48).addBox(-2.0F, 0.0F, -2.0F, 4.0F, 12.0F, 4.0F, new CubeDeformation(0.249F)), PartPose.offset(0.0F, 0.0F, 0.0F));
+
+		PartDefinition upper_body = body_part.addOrReplaceChild("upper_body", CubeListBuilder.create(), PartPose.offset(0.0F, -13.0F, 0.0F));
+
+		PartDefinition right_arms = upper_body.addOrReplaceChild("right_arms", CubeListBuilder.create(), PartPose.offset(-3.5F, -10.0F, 0.0F));
+
+		PartDefinition right_arm = right_arms.addOrReplaceChild("right_arm", CubeListBuilder.create().texOffs(40, 16).addBox(-3.8F, -3.0F, -2.0F, 4.0F, 12.0F, 4.0F, new CubeDeformation(0.0F)), PartPose.offset(-0.7F, 2.0F, 0.0F));
+
+		PartDefinition right_sleeve = right_arms.addOrReplaceChild("right_sleeve", CubeListBuilder.create().texOffs(40, 32).addBox(-3.8F, -3.0F, -2.0F, 4.0F, 12.0F, 4.0F, new CubeDeformation(0.25F)), PartPose.offset(-0.7F, 2.0F, 0.0F));
+
+		PartDefinition left_arms = upper_body.addOrReplaceChild("left_arms", CubeListBuilder.create(), PartPose.offset(3.5F, -10.0F, 0.0F));
+
+		PartDefinition left_arm = left_arms.addOrReplaceChild("left_arm", CubeListBuilder.create().texOffs(32, 48).addBox(-0.2F, -3.0F, -2.0F, 4.0F, 12.0F, 4.0F, new CubeDeformation(0.0F)), PartPose.offset(0.7F, 2.0F, 0.0F));
+
+		PartDefinition left_sleeve = left_arms.addOrReplaceChild("left_sleeve", CubeListBuilder.create().texOffs(48, 48).addBox(-0.2F, -3.0F, -2.0F, 4.0F, 12.0F, 4.0F, new CubeDeformation(0.24F)), PartPose.offset(0.7F, 2.0F, 0.0F));
+
+		PartDefinition body = upper_body.addOrReplaceChild("body", CubeListBuilder.create().texOffs(16, 16).addBox(-4.0F, 0.0F, -2.0F, 8.0F, 12.0F, 4.0F, new CubeDeformation(0.001F)), PartPose.offset(0.0F, -11.0F, 0.0F));
+
+		PartDefinition jacket = upper_body.addOrReplaceChild("jacket", CubeListBuilder.create().texOffs(16, 32).addBox(-4.0F, 0.0F, -2.0F, 8.0F, 12.0F, 4.0F, new CubeDeformation(0.255F)), PartPose.offset(0.0F, -11.0F, 0.0F));
+
+		PartDefinition cloak = upper_body.addOrReplaceChild("cloak", CubeListBuilder.create().texOffs(78, 15).addBox(-5.0F, 0.0F, 2.5F, 10.0F, 16.0F, 0.0F, new CubeDeformation(0.0F)), PartPose.offset(0.0F, -11.0F, 0.0F));
+
+		return LayerDefinition.create(meshdefinition, 128, 128);
+	}
+
+	@Override
+	public void setupAnim(Entity entity, float limbSwing, float limbSwingAmount, float ageInTicks, float netHeadYaw, float headPitch) {
+
+	}
+
+	@Override
+	public void renderToBuffer(PoseStack poseStack, VertexConsumer vertexConsumer, int packedLight, int packedOverlay, float red, float green, float blue, float alpha) {
+		playerlike.render(poseStack, vertexConsumer, packedLight, packedOverlay, red, green, blue, alpha);
+	}
+}
