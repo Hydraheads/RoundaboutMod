a19b81fcfe1e3122a5f67a6f4601ba0fd290e75e
Hydraheads <Hydraheads42@gmail.com>
2025-04-23 20:18:22 -0500

    more modification visage early work

--- common/src/main/java/net/hydra/jojomod/event/powers/visagedata/ModificationVisage.java
+++ common/src/main/java/net/hydra/jojomod/event/powers/visagedata/ModificationVisage.java
@@ -1,9 +1,19 @@
 package net.hydra.jojomod.event.powers.visagedata;
 
+import net.hydra.jojomod.entity.ModEntities;
+import net.hydra.jojomod.entity.visages.JojoNPC;
 import net.minecraft.world.entity.player.Player;
 
 public class ModificationVisage extends NonCharacterVisage {
+    @Override
+    public VisageData generateVisageData(Player entity){
+        return new ModificationVisage(entity);
+    }
     public ModificationVisage(Player self) {
         super(self);
+    }
+    @Override
+    public JojoNPC getModelNPC(Player pl){
+        return ModEntities.DIO.create(pl.level());
     }
 }
--- common/src/main/java/net/hydra/jojomod/event/powers/visagedata/NonCharacterVisage.java
+++ common/src/main/java/net/hydra/jojomod/event/powers/visagedata/NonCharacterVisage.java
@@ -3,6 +3,10 @@ package net.hydra.jojomod.event.powers.visagedata;
 import net.minecraft.world.entity.player.Player;
 
 public class NonCharacterVisage extends VisageData{
+    @Override
+    public VisageData generateVisageData(Player entity){
+        return new NonCharacterVisage(entity);
+    }
     public NonCharacterVisage(Player self) {
         super(self);
     }
