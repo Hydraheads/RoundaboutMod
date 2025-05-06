62b47a77d39bbf06a504aef955acd95e115bf3e1
Hydraheads <Hydraheads42@gmail.com>
2025-05-06 00:14:04 -0500

    ability icons

--- common/src/main/java/net/hydra/jojomod/event/powers/stand/PowersSoftAndWet.java
+++ common/src/main/java/net/hydra/jojomod/event/powers/stand/PowersSoftAndWet.java
@@ -1,13 +1,16 @@
 package net.hydra.jojomod.event.powers.stand;
 
+import net.hydra.jojomod.client.StandIcons;
 import net.hydra.jojomod.entity.ModEntities;
 import net.hydra.jojomod.entity.stand.JusticeEntity;
 import net.hydra.jojomod.entity.stand.StandEntity;
+import net.hydra.jojomod.event.index.PowerIndex;
 import net.hydra.jojomod.event.index.SoundIndex;
 import net.hydra.jojomod.event.powers.StandPowers;
 import net.hydra.jojomod.event.powers.StandUser;
 import net.hydra.jojomod.event.powers.stand.presets.PunchingStand;
 import net.hydra.jojomod.sound.ModSounds;
+import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.sounds.SoundEvent;
 import net.minecraft.world.entity.LivingEntity;
 
@@ -47,6 +50,27 @@ public class PowersSoftAndWet extends PunchingStand {
             return ModSounds.SUMMON_SOFT_AND_WET_EVENT;
         }
         return super.getSoundFromByte(soundChoice);
+    }
+
+    @Override
+    public void renderIcons(GuiGraphics context, int x, int y) {
+
+        if (isHoldingSneak()){
+            setSkillIcon(context, x, y, 1, StandIcons.NONE, PowerIndex.NONE);
+        } else {
+            setSkillIcon(context, x, y, 1, StandIcons.NONE, PowerIndex.NO_CD);
+
+        }
+
+        setSkillIcon(context, x, y, 2, StandIcons.NONE, PowerIndex.SKILL_2);
+
+        if (isHoldingSneak()){
+            setSkillIcon(context, x, y, 3, StandIcons.NONE, PowerIndex.NONE);
+        } else {
+            setSkillIcon(context, x, y, 3, StandIcons.DODGE, PowerIndex.SKILL_3_SNEAK);
+        }
+
+        setSkillIcon(context, x, y, 4, StandIcons.NONE, PowerIndex.SKILL_4);
     }
 }
 
