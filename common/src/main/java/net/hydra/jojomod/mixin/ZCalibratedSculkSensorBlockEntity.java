8662e1bb7f39d0a864bd9e201990180518530fbe
Hydraheads <Hydraheads42@gmail.com>
2025-05-12 01:07:07 -0500

    sculk fixes with bubbles

--- common/src/main/java/net/hydra/jojomod/mixin/ZCalibratedSculkSensorBlockEntity.java
+++ common/src/main/java/net/hydra/jojomod/mixin/ZCalibratedSculkSensorBlockEntity.java
@@ -4,6 +4,9 @@ package net.hydra.jojomod.mixin;
 import net.hydra.jojomod.access.ILevelAccess;
 import net.minecraft.core.BlockPos;
 import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.level.block.entity.BlockEntityType;
+import net.minecraft.world.level.block.entity.SculkSensorBlockEntity;
+import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.gameevent.GameEvent;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -11,7 +14,8 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(targets = "net/minecraft/world/level/block/entity/CalibratedSculkSensorBlockEntity$VibrationUser")
-public class ZCalibratedSculkSensorBlockEntity {
+public abstract class ZCalibratedSculkSensorBlockEntity {
+
     @Inject(method = "canReceiveVibration(Lnet/minecraft/server/level/ServerLevel;Lnet/minecraft/core/BlockPos;Lnet/minecraft/world/level/gameevent/GameEvent;Lnet/minecraft/world/level/gameevent/GameEvent$Context;)Z", at = @At(value = "HEAD"),cancellable = true)
     public void roundabout$canReceiveVibration(ServerLevel $$0, BlockPos $$1, GameEvent $$2, GameEvent.Context $$3, CallbackInfoReturnable<Boolean> cir) {
         if (((ILevelAccess)$$0).roundabout$isSoundPlundered($$1)){
--- common/src/main/java/net/hydra/jojomod/mixin/ZSculkShriekerBlockEntity.java
+++ common/src/main/java/net/hydra/jojomod/mixin/ZSculkShriekerBlockEntity.java
@@ -4,7 +4,12 @@ package net.hydra.jojomod.mixin;
 import net.hydra.jojomod.access.ILevelAccess;
 import net.minecraft.core.BlockPos;
 import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraft.world.level.block.entity.BlockEntityType;
+import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.gameevent.GameEvent;
+import net.minecraft.world.level.gameevent.GameEventListener;
+import net.minecraft.world.level.gameevent.vibrations.VibrationSystem;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -12,7 +17,9 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(targets = "net/minecraft/world/level/block/entity/SculkShriekerBlockEntity$VibrationUser")
-public class ZSculkShriekerBlockEntity {
+public abstract class ZSculkShriekerBlockEntity {
+
+
     @Inject(method = "canReceiveVibration(Lnet/minecraft/server/level/ServerLevel;Lnet/minecraft/core/BlockPos;Lnet/minecraft/world/level/gameevent/GameEvent;Lnet/minecraft/world/level/gameevent/GameEvent$Context;)Z", at = @At(value = "HEAD"),cancellable = true)
     public void roundabout$canReceiveVibration(ServerLevel $$0, BlockPos $$1, GameEvent $$2, GameEvent.Context $$3, CallbackInfoReturnable<Boolean> cir) {
         if (((ILevelAccess)$$0).roundabout$isSoundPlundered($$1)){
