2f7d5dd3500f07060aa93ea5b9c69b29b51d480f
Hydraheads <Hydraheads42@gmail.com>
2025-06-01 11:04:58 -0500

    dummy code bc complicated :(

--- common/src/main/java/net/hydra/jojomod/mixin/ZItemInHandLayer.java
+++ common/src/main/java/net/hydra/jojomod/mixin/ZItemInHandLayer.java
@@ -1,16 +1,24 @@
 package net.hydra.jojomod.mixin;
 
 import com.mojang.blaze3d.vertex.PoseStack;
+import com.mojang.blaze3d.vertex.VertexConsumer;
+import com.mojang.math.Axis;
+import net.hydra.jojomod.Roundabout;
 import net.hydra.jojomod.access.IEntityAndData;
 import net.hydra.jojomod.client.ClientUtil;
+import net.hydra.jojomod.entity.visages.JojoNPC;
 import net.hydra.jojomod.event.powers.StandUser;
 import net.hydra.jojomod.event.powers.TimeStop;
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.model.ArmedModel;
 import net.minecraft.client.model.EntityModel;
 import net.minecraft.client.renderer.MultiBufferSource;
+import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.entity.RenderLayerParent;
 import net.minecraft.client.renderer.entity.layers.ItemInHandLayer;
 import net.minecraft.client.renderer.entity.layers.RenderLayer;
+import net.minecraft.client.renderer.texture.OverlayTexture;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.entity.HumanoidArm;
 import net.minecraft.world.entity.LivingEntity;
@@ -18,8 +26,11 @@ import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemDisplayContext;
 import net.minecraft.world.item.ItemStack;
 import org.jetbrains.annotations.Nullable;
+import org.joml.Matrix4f;
+import org.joml.Vector3f;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
@@ -35,8 +46,18 @@ public class ZItemInHandLayer<T extends LivingEntity, M extends EntityModel<T> &
     public @Nullable ItemStack roundabout$RenderMainHand;
     public @Nullable ItemStack roundabout$RenderOffHand;
     public boolean dominant$Hand;
+
+
+    @Unique
+    private static final ResourceLocation roundabout$TEXTURE_1 = new ResourceLocation(Roundabout.MOD_ID, "textures/stand/soft_and_wet/projectiles/shooting_bubble_1.png");
+    @Unique
+    private static final ResourceLocation roundabout$TEXTURE_2 = new ResourceLocation(Roundabout.MOD_ID, "textures/stand/soft_and_wet/projectiles/shooting_bubble_2.png");
+    @Unique
+    private static final ResourceLocation roundabout$TEXTURE_3 = new ResourceLocation(Roundabout.MOD_ID, "textures/stand/soft_and_wet/projectiles/shooting_bubble_3.png");
+    @Unique
+    private static final ResourceLocation roundabout$TEXTURE_4 = new ResourceLocation(Roundabout.MOD_ID, "textures/stand/soft_and_wet/projectiles/shooting_bubble_4.png");
     @Inject(method = "render", at = @At(value = "HEAD"),cancellable = true)
-    public void roundabout$Render(PoseStack poseStack, MultiBufferSource $$1, int $$2, T Entity, float $$4, float $$5, float $$6, float $$7, float $$8, float $$9, CallbackInfo ci){
+    public void roundabout$Render(PoseStack poseStack, MultiBufferSource buffer, int packedLight, T Entity, float $$4, float $$5, float $$6, float $$7, float $$8, float $$9, CallbackInfo ci){
         dominant$Hand = Entity.getMainArm() == HumanoidArm.RIGHT;
         if (Entity instanceof Player) {
             roundabout$ModifyEntity = ((TimeStop) Entity.level()).CanTimeStopEntity(Entity) || ClientUtil.getScreenFreeze();
@@ -59,7 +80,49 @@ public class ZItemInHandLayer<T extends LivingEntity, M extends EntityModel<T> &
         } else {
             roundabout$ModifyEntity = false;
         }
-        if (((StandUser)Entity).roundabout$getEffectiveCombatMode() && !Entity.isUsingItem()){
+
+        LivingEntity host = Entity;
+        if (Entity instanceof JojoNPC jnpc && ((StandUser)jnpc.host).roundabout$getEffectiveCombatMode()){
+            host = jnpc.host;
+        }
+        if (host != null && ((StandUser)host).roundabout$getEffectiveCombatMode() && !host.isUsingItem()){
+
+            /** Ultimately rendering the hand bubble in 2D is inconvenient because of all the places it has to be done,
+             * so I wish to instead burn a new layer in 3d onto the hand
+             *
+            poseStack.pushPose();
+            HumanoidArm mainArm = host.getMainArm();
+            this.getParentModel().translateToHand(mainArm, poseStack);
+            poseStack.mulPose(Axis.XP.rotationDegrees(-90.0F));
+            poseStack.mulPose(Axis.YP.rotationDegrees(180.0F));
+            boolean humanarm = mainArm == HumanoidArm.LEFT;
+            poseStack.translate((float)(humanarm ? -1 : 1) / 16.0F, 0.125F, -0.625F);
+            poseStack.translate(1,0,0);
+
+
+            // Draw flat quad here
+            Vector3f normal = Minecraft.getInstance().gameRenderer.getMainCamera().getLookVector();
+            normal.normalize();
+            Vector3f coursecorrect = new Vector3f(0.577f, 0.577f, 0.577f);
+            if (normal.y > 0) {
+                coursecorrect = new Vector3f(0.01f, 1f, 0.01f);
+            }
+
+            // Create vertices
+            Matrix4f matrix = poseStack.last().pose();
+            VertexConsumer vertexConsumer = buffer.getBuffer(RenderType.entityTranslucent(roundabout$TEXTURE_1));
+            float size = 1f;
+
+            // Use a simple normal (optional, mostly lighting-related)
+            vertexConsumer.vertex(matrix, -size, -size, 0.0f).color(255, 255, 255, 255).uv(0.0f, 1.0f).overlayCoords(OverlayTexture.NO_OVERLAY).uv2(packedLight).normal(coursecorrect.x, coursecorrect.y, coursecorrect.z).endVertex();
+            vertexConsumer.vertex(matrix, size, -size, 0.0f).color(255, 255, 255, 255).uv(1.0f, 1.0f).overlayCoords(OverlayTexture.NO_OVERLAY).uv2(packedLight).normal(coursecorrect.x, coursecorrect.y, coursecorrect.z).endVertex();
+            vertexConsumer.vertex(matrix, size, size, 0.0f).color(255, 255, 255, 255).uv(1.0f, 0.0f).overlayCoords(OverlayTexture.NO_OVERLAY).uv2(packedLight).normal(coursecorrect.x, coursecorrect.y, coursecorrect.z).endVertex();
+            vertexConsumer.vertex(matrix, -size, size, 0.0f).color(255, 255, 255, 255).uv(0.0f, 0.0f).overlayCoords(OverlayTexture.NO_OVERLAY).uv2(packedLight).normal(coursecorrect.x, coursecorrect.y, coursecorrect.z).endVertex();
+
+
+            poseStack.popPose();
+             **/
+
             ci.cancel();
             return;
         }
