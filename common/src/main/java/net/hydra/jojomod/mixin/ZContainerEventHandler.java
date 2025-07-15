cb0c1c4887b6e6a340953f5ece574335bf88a980
Hydraheads <Hydraheads42@gmail.com>
2025-07-14 22:43:04 -0500

    fixed button

--- common/src/main/java/net/hydra/jojomod/mixin/ZContainerEventHandler.java
+++ common/src/main/java/net/hydra/jojomod/mixin/ZContainerEventHandler.java
@@ -18,26 +18,4 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 @Mixin(ContainerEventHandler.class)
 public interface ZContainerEventHandler {
 
-    @Inject(method = "mouseClicked", at = @At("HEAD"), cancellable = true)
-    private void roundabout$mouseClicked(double mouseX, double mouseY, int mouseButton, CallbackInfoReturnable<Boolean> cir)
-    {
-        if (!(((ContainerEventHandler)(Object)this) instanceof OptionsScreen))
-            return;
-
-        if (!ClientConfig.getLocalInstance().configSettings.shouldShowConfigButton)
-            return;
-
-        if (mouseButton == 0 && ClientUtil.roundabout$configButtonSelected)
-        {
-            ClientUtil.roundabout$configButtonSelected = false;
-            Minecraft client = Minecraft.getInstance();
-            client.setScreen(new ConfigScreen(ConfigType.COMMON, client.screen));
-
-            SoundManager soundmanager = Minecraft.getInstance().getSoundManager();
-            soundmanager.play(SimpleSoundInstance.forUI(SoundEvents.UI_BUTTON_CLICK, 1.0F));
-            cir.setReturnValue(true);
-            cir.cancel();
-            ClientUtil.roundabout$configButtonSelected = false;
-        }
-    }
 }
--- common/src/main/java/net/hydra/jojomod/mixin/ZOptionsScreen.java
+++ common/src/main/java/net/hydra/jojomod/mixin/ZOptionsScreen.java
@@ -2,12 +2,20 @@ package net.hydra.jojomod.mixin;
 
 import com.mojang.blaze3d.systems.RenderSystem;
 import net.hydra.jojomod.Roundabout;
+import net.hydra.jojomod.client.ClientNetworking;
 import net.hydra.jojomod.client.ClientUtil;
+import net.hydra.jojomod.client.gui.config.ConfigScreen;
+import net.hydra.jojomod.client.gui.config.ConfigType;
 import net.hydra.jojomod.util.config.ClientConfig;
+import net.minecraft.ChatFormatting;
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiGraphics;
+import net.minecraft.client.gui.components.Button;
+import net.minecraft.client.gui.layouts.GridLayout;
 import net.minecraft.client.gui.screens.OptionsScreen;
 import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.client.gui.screens.TitleScreen;
+import net.minecraft.network.chat.CommonComponents;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
 import org.spongepowered.asm.mixin.Final;
@@ -16,6 +24,7 @@ import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
 @Mixin(OptionsScreen.class)
 public abstract class ZOptionsScreen extends Screen {
@@ -27,50 +36,22 @@ public abstract class ZOptionsScreen extends Screen {
         super($$0);
     }
 
-    @Inject(method = "render", at = @At("TAIL"))
-    private void roundabout$finishRender(GuiGraphics drawContext, int mouseX, int mouseY, float tickDelta, CallbackInfo ci)
+    @Inject(method = "init", at = @At(value = "INVOKE",
+            target = "Lnet/minecraft/client/gui/layouts/GridLayout;arrangeElements()V",
+            shift = At.Shift.BEFORE),
+            locals = LocalCapture.CAPTURE_FAILHARD)
+    private void roundabout$init(CallbackInfo ci, GridLayout $$0, GridLayout.RowHelper $$1)
     {
         if (!ClientConfig.getLocalInstance().configSettings.shouldShowConfigButton || !(this.lastScreen instanceof TitleScreen))
             return;
-
-        ResourceLocation BUTTON_ICON = new ResourceLocation(Roundabout.MOD_ID, "textures/item/stand_arrow.png");
-        ResourceLocation BUTTON_BACKGROUND = new ResourceLocation(Roundabout.MOD_ID, "textures/gui/blank_button.png");
-
-        int iconX = (drawContext.guiWidth() / 2) + 124 -20;
-        int iconY = ((drawContext.guiHeight() / 4) + 48) + 50 + 12 + 37;
-
-        iconX += ClientConfig.getLocalInstance().configSettings.configButtonOffsetX;
-        iconY += ClientConfig.getLocalInstance().configSettings.configButtonOffsetY;
-
-        ClientUtil.roundabout$configButtonSelected = false;
-
-        if (mouseX >= iconX && mouseY >= iconY)
-        {
-            if (mouseX <= iconX + 20 && mouseY <= iconY + 20)
-            {
-                ClientUtil.roundabout$configButtonSelected = true;
-            }
-        }
-
-        //float fadeTransparency = fading ? (float)(Util.getMillis() - fadeInStart) / 1000.0F : 1.0F;
-        //float fadeAlpha = this.fading ? Mth.clamp(fadeTransparency - 1.0F, 0.0F, 1.0F) : 1.0F;
-        float fadeAlpha = 1;
-
-        RenderSystem.enableBlend();
-        drawContext.setColor(1.0f, 1.0f, 1.0f, fadeAlpha);
-
-        drawContext.blit(BUTTON_BACKGROUND,
-                iconX, // x
-                iconY, // y
-                20, // width
-                20, // height
-                0, // u
-                ClientUtil.roundabout$configButtonSelected ? 20 : 0, // v
-                20, // regionWidth
-                20, // regionHeight
-                64, // textureWidth
-                64); // textureHeight
-
-        drawContext.blit(BUTTON_ICON, iconX + 2, iconY + 2, 0, 0, 16, 16, 16, 16);
+        Minecraft client = Minecraft.getInstance();
+        $$1.addChild(
+                Button.builder(Component.translatable("config.roundabout.roundaboutconfig.name").withStyle(ChatFormatting.LIGHT_PURPLE),
+                        $$0x -> this.minecraft.setScreen(new ConfigScreen(ConfigType.COMMON, client.screen))).
+                        width(50).build(), 2,
+                $$1.newCellSettings().paddingTop(-24+(ClientConfig.getLocalInstance().configSettings.configButtonOffsetY)).alignHorizontallyRight().
+                        paddingRight(5+(ClientConfig.getLocalInstance().configSettings.configButtonOffsetX))
+        );
     }
+
 }
