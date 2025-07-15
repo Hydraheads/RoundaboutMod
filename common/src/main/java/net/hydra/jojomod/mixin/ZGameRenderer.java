53c7345626fe92a74060f2fa28a54d4c9534fb99
CMDR-3 <supernovaplayz04@gmail.com>
2025-07-15 16:09:59 -0700

    fix a critical flaw with post shaders (boredom)

--- common/src/main/java/net/hydra/jojomod/mixin/ZGameRenderer.java
+++ common/src/main/java/net/hydra/jojomod/mixin/ZGameRenderer.java
@@ -32,20 +32,21 @@ public abstract class ZGameRenderer {
     @Inject(method = "renderLevel", at= @At(value = "INVOKE", target = "Lcom/mojang/blaze3d/systems/RenderSystem;clear(IZ)V", shift = At.Shift.BEFORE))
     private void roundabout$beforeClearDepthBuffer(float tickDelta, long $$1, PoseStack $$2, CallbackInfo ci)
     {
+        // TODO: copy the old depth buffer to a texture
+    }
+
+    @Inject(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/renderer/LevelRenderer;doEntityOutline()V", shift = At.Shift.AFTER))
+    private void roundabout$renderShaders(float tickDelta, long $$1, boolean renderLevel, CallbackInfo ci)
+    {
         RenderCallbackRegistry.roundabout$GAME_RENDERER_FINISH(tickDelta);
 
         if (RPostShaderRegistry.DESATURATE != null) {
             if (((IGameRenderer)Minecraft.getInstance().gameRenderer).roundabout$tsShaderStatus())
             {
+                RPostShaderRegistry.DESATURATE.roundabout$setUniform("InvProjMat", RPostShaderRegistry.InverseProjectionMatrix);
                 RPostShaderRegistry.DESATURATE.roundabout$process(tickDelta);
             }
         }
-
-//        if (RPostShaderRegistry.D4C_DIMENSION_TRANSITION != null)
-//        {
-//            RPostShaderRegistry.D4C_DIMENSION_TRANSITION.roundabout$resize();
-//            ((PostChain)RPostShaderRegistry.D4C_DIMENSION_TRANSITION).process(tickDelta);
-//        }
     }
 
     @Inject(method = "reloadShaders", at=@At("HEAD"))
