f45429903ae76660836683453a90551197604b27
Hydraheads <Hydraheads42@gmail.com>
2025-07-17 09:05:27 -0500

    fixed harpoon model handling invis poorly

--- bbmodels/harpoon.java
+++ bbmodels/harpoon.java
@@ -1,33 +1,15 @@
-// Made with Blockbench 4.10.4
+// Made with Blockbench 4.12.5
 // Exported for Minecraft version 1.17 or later with Mojang mappings
 // Paste this class into your mod and generate all required imports
-package net.hydra.jojomod.client.models.projectile;
 
-import com.mojang.blaze3d.vertex.PoseStack;
-import com.mojang.blaze3d.vertex.VertexConsumer;
-import net.hydra.jojomod.Roundabout;
-import net.hydra.jojomod.client.ModItemModels;
-import net.minecraft.client.model.EntityModel;
-import net.minecraft.client.model.Model;
-import net.minecraft.client.model.geom.ModelLayerLocation;
-import net.minecraft.client.model.geom.ModelPart;
-import net.minecraft.client.model.geom.PartPose;
-import net.minecraft.client.model.geom.builders.*;
-import net.minecraft.client.renderer.RenderType;
-import net.minecraft.resources.ResourceLocation;
 
-import javax.swing.text.html.parser.Entity;
-
-public class HarpoonModel extends Model {
+public class harpoon<T extends Entity> extends EntityModel<T> {
 	// This layer location should be baked with EntityRendererProvider.Context in the entity renderer and passed into this model's constructor
-	public static final ModelLayerLocation LAYER_LOCATION = new ModelLayerLocation(new ResourceLocation(Roundabout.MOD_ID, "harpoon"), "main");
+	public static final ModelLayerLocation LAYER_LOCATION = new ModelLayerLocation(new ResourceLocation("modid", "harpoon"), "main");
 	private final ModelPart harpoon;
 
-	public HarpoonModel(ModelPart root) {
-
-		super(RenderType::entityCutout);
-		this.harpoon = root;
-		ModItemModels.HARPOON_MODEL = this;
+	public harpoon(ModelPart root) {
+		this.harpoon = root.getChild("harpoon");
 	}
 
 	public static LayerDefinition createBodyLayer() {
@@ -35,14 +17,19 @@ public class HarpoonModel extends Model {
 		PartDefinition partdefinition = meshdefinition.getRoot();
 
 		PartDefinition harpoon = partdefinition.addOrReplaceChild("harpoon", CubeListBuilder.create().texOffs(13, 13).addBox(0.575F, -2.0F, -1.0F, 1.0F, 14.0F, 1.0F, new CubeDeformation(0.0F))
-				.texOffs(0, -7).addBox(1.075F, -9.0F, -4.0F, 0.0F, 8.0F, 7.0F, new CubeDeformation(0.0F))
-				.texOffs(0, 0).addBox(-2.425F, -9.0F, -0.475F, 7.0F, 8.0F, 0.0F, new CubeDeformation(0.0F)), PartPose.offset(-0.575F, 3.0F, 0.0F));
+		.texOffs(0, -7).addBox(1.075F, -9.0F, -4.0F, 0.0F, 8.0F, 7.0F, new CubeDeformation(0.0F))
+		.texOffs(0, 0).addBox(-2.425F, -9.0F, -0.475F, 7.0F, 8.0F, 0.0F, new CubeDeformation(0.0F)), PartPose.offset(-0.575F, 3.0F, 0.0F));
 
 		PartDefinition cube_r1 = harpoon.addOrReplaceChild("cube_r1", CubeListBuilder.create().texOffs(0, 0).addBox(-3.5F, -8.0F, 0.0F, 7.0F, 8.0F, 0.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(1.075F, -1.0F, -0.55F, 0.0F, 3.1416F, 0.0F));
 
 		PartDefinition cube_r2 = harpoon.addOrReplaceChild("cube_r2", CubeListBuilder.create().texOffs(0, 0).addBox(-3.5F, -8.0F, 0.0F, 7.0F, 8.0F, 0.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(1.075F, -1.0F, -0.5F, 0.0F, 3.1416F, 0.0F));
 
 		return LayerDefinition.create(meshdefinition, 32, 32);
+	}
+
+	@Override
+	public void setupAnim(Entity entity, float limbSwing, float limbSwingAmount, float ageInTicks, float netHeadYaw, float headPitch) {
+
 	}
 
 	@Override
