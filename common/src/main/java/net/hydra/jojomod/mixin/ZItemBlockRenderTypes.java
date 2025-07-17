85ffda2661d4e6a2ab228898da5b4694078ef927
Hydraheads <Hydraheads42@gmail.com>
2025-07-16 21:14:05 -0500

    see through items

---
+++ common/src/main/java/net/hydra/jojomod/mixin/ZItemBlockRenderTypes.java
@@ -0,0 +1,25 @@
+package net.hydra.jojomod.mixin;
+
+import com.mojang.blaze3d.vertex.PoseStack;
+import net.hydra.jojomod.client.ClientUtil;
+import net.minecraft.client.renderer.ItemBlockRenderTypes;
+import net.minecraft.client.renderer.MultiBufferSource;
+import net.minecraft.client.renderer.RenderType;
+import net.minecraft.client.renderer.Sheets;
+import net.minecraft.world.item.ItemStack;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(ItemBlockRenderTypes.class)
+public abstract class ZItemBlockRenderTypes {
+    @Inject(method = "getRenderType(Lnet/minecraft/world/item/ItemStack;Z)Lnet/minecraft/client/renderer/RenderType;", at = @At(value = "HEAD"), cancellable = true)
+    private static void roundabout$getRenderTypeItems(ItemStack $$0, boolean $$1, CallbackInfoReturnable<RenderType> cir) {
+        float throwfade = ClientUtil.getThrowFadeToTheEther();
+        if (throwfade != 1) {
+            cir.setReturnValue(Sheets.translucentItemSheet());
+        }
+    }
+}
