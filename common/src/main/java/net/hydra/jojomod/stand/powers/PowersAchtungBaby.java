1ffea732bdea46f65e3e495350dc04c58d2929ef
Hydraheads <Hydraheads42@gmail.com>
2025-07-19 00:26:46 -0500

    invisible block placement + translation

--- common/src/main/java/net/hydra/jojomod/stand/powers/PowersAchtungBaby.java
+++ common/src/main/java/net/hydra/jojomod/stand/powers/PowersAchtungBaby.java
@@ -1,7 +1,6 @@
 package net.hydra.jojomod.stand.powers;
 
 import com.google.common.collect.Lists;
-import net.hydra.jojomod.Roundabout;
 import net.hydra.jojomod.access.IEntityAndData;
 import net.hydra.jojomod.block.InvisiBlockEntity;
 import net.hydra.jojomod.block.ModBlocks;
@@ -21,6 +20,7 @@ import net.hydra.jojomod.util.MainUtil;
 import net.minecraft.ChatFormatting;
 import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.network.chat.Component;
 import net.minecraft.server.level.ServerLevel;
@@ -33,13 +33,22 @@ import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.Mob;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.*;
+import net.minecraft.world.item.context.BlockPlaceContext;
+import net.minecraft.world.item.context.DirectionalPlaceContext;
+import net.minecraft.world.level.ClipContext;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.RotatedPillarBlock;
+import net.minecraft.world.level.block.ShulkerBoxBlock;
+import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.gameevent.GameEvent;
+import net.minecraft.world.phys.BlockHitResult;
 import net.minecraft.world.phys.Vec3;
+import net.minecraft.world.phys.shapes.CollisionContext;
 
-import java.util.ArrayList;
-import java.util.Arrays;
+import javax.annotation.Nullable;
 import java.util.List;
 
 public class PowersAchtungBaby extends NewDashPreset {
@@ -73,7 +82,15 @@ public class PowersAchtungBaby extends NewDashPreset {
         else
             setSkillIcon(context, x, y, 2, StandIcons.BURST_INVIS, PowerIndex.SKILL_2);
 
-        setSkillIcon(context, x, y, 3, StandIcons.DODGE, PowerIndex.GLOBAL_DASH);
+        if (isHoldingSneak())
+            setSkillIcon(context, x, y, 2, StandIcons.SELF_INVIS, PowerIndex.SKILL_2);
+        else
+            setSkillIcon(context, x, y, 2, StandIcons.BURST_INVIS, PowerIndex.SKILL_2);
+
+        if (isHoldingSneak() && ClientNetworking.getAppropriateConfig().achtungSettings.hidesPlacedBlocks)
+            setSkillIcon(context, x, y, 3, StandIcons.INVIS_BLOCK, PowerIndex.SKILL_3);
+        else
+            setSkillIcon(context, x, y, 3, StandIcons.DODGE, PowerIndex.GLOBAL_DASH);
 
         super.renderIcons(context, x, y);
     }
@@ -106,12 +123,44 @@ public class PowersAchtungBaby extends NewDashPreset {
             case SKILL_2_CROUCH-> {
                 invisiburstSimpleClient();
             }
-            case SKILL_3_NORMAL, SKILL_3_CROUCH -> {
+            case SKILL_3_NORMAL -> {
                 dash();
+            }
+            case SKILL_3_CROUCH -> {
+                placeOrDashClient();
             }
         }
     }
 
+    public void placeOrDashClient(){
+        if (ClientNetworking.getAppropriateConfig().achtungSettings.hidesPlacedBlocks) {
+            placeClient();
+        } else {
+            dash();
+        }
+    }
+    public void placeClient(){
+        ItemStack stack = this.getSelf().getMainHandItem();
+        if (!onCooldown(PowerIndex.SKILL_3)) {
+            this.setCooldown(PowerIndex.SKILL_3, 2);
+            if (!stack.isEmpty()) {
+                if (((IEntityAndData) this.self).roundabout$getTrueInvisibility() > -1) {
+                    ((StandUser) this.getSelf()).roundabout$tryIntPower(PowerIndex.POWER_3, true,
+                            ((Player) this.getSelf()).getInventory().selected);
+                    tryIntPowerPacket(PowerIndex.POWER_3,
+                            ((Player) this.getSelf()).getInventory().selected);
+                }
+            }
+        }
+    }
+    public int grabInventorySlot=1;
+    @Override
+    public boolean tryIntPower(int move, boolean forced, int chargeTime){
+        if (move == PowerIndex.POWER_3){
+            this.grabInventorySlot = chargeTime;
+        }
+        return super.tryIntPower(move, forced, chargeTime);
+    }
     public int burstTicks = -1;
     public boolean inBurstState(){
         return burstTicks > 1;
@@ -150,11 +199,139 @@ public class PowersAchtungBaby extends NewDashPreset {
             case PowerIndex.POWER_2_SNEAK -> {
                 return invisibleBurstSimple();
             }
+            case PowerIndex.POWER_3 -> {
+                return inventoryGrab();
+            }
         }
         return super.setPowerOther(move,lastMove);
     }
 
 
+    public boolean inventoryGrab() {
+        if (!this.getSelf().level().isClientSide()) {
+            if (this.getSelf() instanceof Player) {
+                if (((IEntityAndData) this.self).roundabout$getTrueInvisibility() > -1) {
+                    ItemStack stack = ((Player) this.getSelf()).getInventory().getItem(this.grabInventorySlot);
+                    if (!stack.isEmpty() && stack.getItem() instanceof BlockItem BE && !(
+                            BE.getBlock() instanceof ShulkerBoxBlock)) {
+                        ItemStack stack2 = stack.copy();
+
+                        if (placeBlock(stack2))
+                            stack.shrink(1);
+
+
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+
+    @SuppressWarnings("deprecation")
+    public boolean placeBlock(ItemStack stack) {
+        if (!this.getSelf().level().isClientSide()) {
+                Vec3 vec3d = this.getSelf().getEyePosition(0);
+                Vec3 vec3d2 = this.getSelf().getViewVector(0);
+                Vec3 vec3d3 = vec3d.add(vec3d2.x * 5, vec3d2.y * 5, vec3d2.z * 5);
+                BlockHitResult $$0 = this.getSelf().level().clip(new ClipContext(vec3d, vec3d3,
+                        ClipContext.Block.COLLIDER, ClipContext.Fluid.NONE, this.getSelf()));
+                BlockPos pos = null;
+                if (this.getSelf().level().getBlockState($$0.getBlockPos()).isSolid() ||
+                        this.getSelf().level().getBlockState($$0.getBlockPos()).is(ModBlocks.INVISIBLOCK)){
+                    pos = $$0.getBlockPos().relative($$0.getDirection());
+                }
+                if (pos != null) {
+                    BlockState state = this.getSelf().level().getBlockState(pos);
+                    if (stack.getItem() instanceof BlockItem blockItem) {
+                        if (MainUtil.getIsGamemodeApproriateForGrief(this.self)) {
+                            if(this.getSelf() instanceof Player plr) {
+                                if(!MainUtil.canPlaceOnClaim(plr,$$0)){
+                                    return false;
+                                }
+                            }
+
+                            if (tryHitBlockAndConvert($$0, pos, state, stack)) {
+                                this.setCooldown(PowerIndex.SKILL_3, 3);
+                                return true;
+                            }
+                        }
+                    }
+                }
+        }
+        return false;
+    }
+
+    protected boolean canPlace(BlockPlaceContext $$0, BlockState $$1, BlockItem stack) {
+        Player $$2 = $$0.getPlayer();
+        CollisionContext $$3 = $$2 == null ? CollisionContext.empty() : CollisionContext.of($$2);
+        return ($$1.canSurvive($$0.getLevel(), $$0.getClickedPos())) && $$0.getLevel().isUnobstructed($$1, $$0.getClickedPos(), $$3);
+    }
+
+    @Nullable
+    protected BlockState getPlacementState(BlockPlaceContext $$0, BlockItem stack) {
+        BlockState $$1 = stack.getBlock().getStateForPlacement($$0);
+        return $$1 != null && this.canPlace($$0, $$1,stack) ? $$1 : null;
+    }
+    public boolean tryHitBlockAndConvert(BlockHitResult $$0, BlockPos pos, BlockState state, ItemStack stack){
+
+        if ((state.isAir() || state.canBeReplaced()) && !((this.getSelf() instanceof Player &&
+                (((Player) this.getSelf()).blockActionRestricted(this.getSelf().level(), pos, ((ServerPlayer)
+                        this.getSelf()).gameMode.getGameModeForPlayer()))) ||
+                !this.getSelf().level().mayInteract(((Player) this.getSelf()), pos))){
+
+            if (stack.getItem() instanceof BlockItem BI) {
+                Direction direction = $$0.getDirection();
+                if (direction.getAxis() == Direction.Axis.X){
+                    direction = direction.getOpposite();
+                }
+                if (BI.getBlock() instanceof RotatedPillarBlock){
+                    direction = $$0.getDirection();
+                }
+
+                DirectionalPlaceContext dpc = new DirectionalPlaceContext(this.getSelf().level(),
+                        pos,
+                        direction, stack,
+                        direction);
+                if (dpc.canPlace()) {
+
+                    BlockState $$2 = this.getPlacementState(dpc, BI);
+                    if ($$2 != null) {
+                        BlockPlaceContext $$1 = this.updatePlacementContext(dpc);
+                        if (this.self.level().setBlock($$1.getClickedPos(), ModBlocks.INVISIBLOCK.defaultBlockState(), 11)) {
+                            BlockEntity BE = this.self.level().getBlockEntity($$1.getClickedPos());
+                            if (BE instanceof InvisiBlockEntity IBE) {
+
+
+                                BlockPos $$3 = $$1.getClickedPos();
+                                Level $$4 = $$1.getLevel();
+                                Player $$5 = $$1.getPlayer();
+                                BlockState $$7 = $$4.getBlockState($$3);
+                                SoundType $$8 = $$7.getSoundType();
+                                $$4.playSound($$5, $$3, this.getPlaceSound(BI.getBlock().defaultBlockState()), SoundSource.BLOCKS, ($$8.getVolume() + 1.0F) / 2.0F, $$8.getPitch() * 0.8F);
+                                $$4.gameEvent(GameEvent.BLOCK_PLACE, $$3, GameEvent.Context.of($$5, $$7));
+
+                                IBE.setOriginal2(BI.getBlock().getStateForPlacement($$1));
+                                IBE.ticksUntilRestore = ((IEntityAndData) this.self).roundabout$getTrueInvisibility();
+                                PowersAchtungBaby.spawnExplosionParticles(this.self.level(), pos.getCenter(), 10, 0.2);
+                                return true;
+                            }
+                        }
+                    }
+                }
+            }
+
+        }
+        return false;
+    }
+
+    @Nullable
+    public BlockPlaceContext updatePlacementContext(BlockPlaceContext $$0) {
+        return $$0;
+    }
+    protected SoundEvent getPlaceSound(BlockState $$0) {
+        return $$0.getSoundType().getPlaceSound();
+    }
     public boolean isServerControlledCooldown(CooldownInstance ci, byte num){
         if (num == PowerIndex.SKILL_2 && ClientNetworking.getAppropriateConfig().achtungSettings.invisiBurstCooldownUsesServerLatency) {
             return true;
@@ -162,6 +339,14 @@ public class PowersAchtungBaby extends NewDashPreset {
         return super.isServerControlledCooldown(ci, num);
     }
 
+
+    public boolean isPlacable(ItemStack stack){
+        if (stack != null && !stack.isEmpty() && stack.getItem() instanceof BlockItem BE)
+            return true;
+        return false;
+    }
+
+    @SuppressWarnings("deprecation")
     public void burstBlocks(ServerLevel sl){
         int radius = ClientNetworking.getAppropriateConfig().achtungSettings.invisiBurstBlockRange;
         BlockPos baseCenter = this.self.getOnPos();
@@ -428,6 +613,10 @@ public class PowersAchtungBaby extends NewDashPreset {
 
 
     public boolean isAttackIneptVisually(byte activeP, int slot) {
+        if (ClientNetworking.getAppropriateConfig().achtungSettings.hidesPlacedBlocks) {
+            if (slot == 3 && isHoldingSneak() && ((IEntityAndData) this.self).roundabout$getTrueInvisibility() <= -1)
+                return true;
+        }
         return super.isAttackIneptVisually(activeP,slot);
     }
 
@@ -443,6 +632,10 @@ public class PowersAchtungBaby extends NewDashPreset {
                 "instruction.roundabout.press_skill_crouch", StandIcons.SELF_INVIS,3,level,bypass));
         $$1.add(drawSingleGUIIcon(context, 18, leftPos + 39, topPos + 80, 0, "ability.roundabout.dodge",
                 "instruction.roundabout.press_skill", StandIcons.DODGE,3,level,bypass));
+        if (ClientNetworking.getAppropriateConfig().achtungSettings.hidesPlacedBlocks) {
+            $$1.add(drawSingleGUIIcon(context, 18, leftPos + 39, topPos + 99, 0, "ability.roundabout.place_invis",
+                    "instruction.roundabout.press_skill_crouch", StandIcons.INVIS_BLOCK, 3, level, bypass));
+        }
         return $$1;
     }
 
--- common/src/main/java/net/hydra/jojomod/util/config/Config.java
+++ common/src/main/java/net/hydra/jojomod/util/config/Config.java
@@ -545,7 +545,7 @@ public class Config implements Cloneable {
         public Boolean hidesHeldItems;
         @BooleanOption(group = "inherit", value = true)
         public Boolean hidesShotProjectiles;
-        @BooleanOption(group = "inherit", value = false)
+        @BooleanOption(group = "inherit", value = true)
         public Boolean hidesPlacedBlocks;
         @BooleanOption(group = "inherit", value = true)
         public Boolean revealLocationWhenDamagingOrHurt;
