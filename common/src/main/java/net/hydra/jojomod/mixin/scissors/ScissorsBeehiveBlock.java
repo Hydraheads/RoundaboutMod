de2903d994a9a5391a9a0d41312f373d73e7f445
Hydraheads <Hydraheads42@gmail.com>
2025-07-20 17:58:58 -0500

    fixed minor aggro mixin mistake

--- common/src/main/java/net/hydra/jojomod/mixin/scissors/ScissorsBeehiveBlock.java
+++ common/src/main/java/net/hydra/jojomod/mixin/scissors/ScissorsBeehiveBlock.java
@@ -1,4 +1,4 @@
-package net.hydra.jojomod.mixin;
+package net.hydra.jojomod.mixin.scissors;
 
 import net.hydra.jojomod.item.ModItems;
 import net.minecraft.core.BlockPos;
@@ -14,7 +14,6 @@ import net.minecraft.world.level.block.BeehiveBlock;
 import net.minecraft.world.level.block.CampfireBlock;
 import net.minecraft.world.level.block.entity.BeehiveBlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
-import net.minecraft.world.level.block.state.properties.BlockStateProperties;
 import net.minecraft.world.level.block.state.properties.IntegerProperty;
 import net.minecraft.world.level.gameevent.GameEvent;
 import net.minecraft.world.phys.BlockHitResult;
@@ -27,26 +26,12 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(BeehiveBlock.class)
-public class ZBeehiveBlock {
-    /**Scissors*/
-    @Shadow
-    @Final
-    public static IntegerProperty HONEY_LEVEL;
-    @Shadow
-    public void resetHoneyLevel(Level level, BlockState blockState, BlockPos blockPos){
-    }
-    @Shadow
-    private boolean hiveContainsBees(Level level, BlockPos blockPos){
-        return false;
-    }
-
-    @Shadow
-    private void angerNearbyBees(Level level, BlockPos blockPos){
-    }
-
-    @Shadow
-    public void releaseBeesAndResetHoneyLevel(Level level, BlockState blockState, BlockPos blockPos, @Nullable Player player, BeehiveBlockEntity.BeeReleaseStatus beeReleaseStatus) {
-    }
+public class ScissorsBeehiveBlock {
+    /**This mixin is for scissors being usuable on beehive blocks
+     * For context, shears are usable in this regard, and
+     * scissors double as shears in a lot of ways.
+     *
+     * The code is mostly just a faithful use of vanilla code so if it updates check this out.*/
 
     @Inject(method = "use", at = @At(value = "HEAD"), cancellable = true)
     protected void roundabout$Tick(BlockState $$0, Level $$1, BlockPos $$2, Player $$3, InteractionHand $$4, BlockHitResult $$5, CallbackInfoReturnable<InteractionResult> cir) {
@@ -75,5 +60,30 @@ public class ZBeehiveBlock {
 
             }
         }
+    }
+
+
+    /**Shadows, ignore
+     * -------------------------------------------------------------------------------------------------------------
+     * */
+
+
+    @Shadow
+    @Final
+    public static IntegerProperty HONEY_LEVEL;
+    @Shadow
+    public void resetHoneyLevel(Level level, BlockState blockState, BlockPos blockPos){
+    }
+    @Shadow
+    private boolean hiveContainsBees(Level level, BlockPos blockPos){
+        return false;
+    }
+
+    @Shadow
+    private void angerNearbyBees(Level level, BlockPos blockPos){
+    }
+
+    @Shadow
+    public void releaseBeesAndResetHoneyLevel(Level level, BlockState blockState, BlockPos blockPos, @Nullable Player player, BeehiveBlockEntity.BeeReleaseStatus beeReleaseStatus) {
     }
 }
