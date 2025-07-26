d2f981829f364ffd6b8ec4994b006c21bbc158d5
Hydraheads <Hydraheads42@gmail.com>
2025-07-26 10:28:05 -0500

    fixed crop/chorus breaking with achtung

--- common/src/main/java/net/hydra/jojomod/mixin/achtung/AchtungChorusFlowerBlock.java
+++ common/src/main/java/net/hydra/jojomod/mixin/achtung/AchtungChorusFlowerBlock.java
@@ -4,18 +4,18 @@ import net.hydra.jojomod.block.ModBlocks;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.LevelReader;
-import net.minecraft.world.level.block.BushBlock;
+import net.minecraft.world.level.block.ChorusFlowerBlock;
 import net.minecraft.world.level.block.state.BlockState;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(BushBlock.class)
-public class AchtungBushBlock {
+@Mixin(ChorusFlowerBlock.class)
+public class AchtungChorusFlowerBlock {
 
-    /**The invisible blocks of achtung baby should not be breaking tallgrass or plants*/
-    @Inject(method = "canSurvive", at = @At("HEAD"), cancellable = true)
+    /**The invisible blocks of achtung baby should not be breaking chorus plants*/
+    @Inject(method = "canSurvive(Lnet/minecraft/world/level/block/state/BlockState;Lnet/minecraft/world/level/LevelReader;Lnet/minecraft/core/BlockPos;)Z", at = @At("HEAD"), cancellable = true)
     public void roundabout$canSurvive(BlockState $$0, LevelReader $$1, BlockPos $$2, CallbackInfoReturnable<Boolean> cir) {
         BlockState blockstate = $$1.getBlockState($$2.below());
         if (blockstate.is(ModBlocks.INVISIBLOCK)){
