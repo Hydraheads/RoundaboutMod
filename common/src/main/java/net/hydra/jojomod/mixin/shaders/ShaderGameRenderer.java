de67beb96f156c5ae785c2c80e126772446c55eb
Hydraheads <Hydraheads42@gmail.com>
2025-08-04 14:00:22 -0500

    merged a mixin

--- common/src/main/java/net/hydra/jojomod/mixin/shaders/ShaderGameRenderer.java
+++ common/src/main/java/net/hydra/jojomod/mixin/shaders/ShaderGameRenderer.java
@@ -1,6 +1,7 @@
 package net.hydra.jojomod.mixin.shaders;
 
 import com.mojang.blaze3d.vertex.PoseStack;
+import net.hydra.jojomod.Roundabout;
 import net.hydra.jojomod.access.IShaderGameRenderer;
 import net.hydra.jojomod.client.ClientUtil;
 import net.hydra.jojomod.event.powers.TimeStop;
@@ -10,8 +11,13 @@ import net.minecraft.client.CameraType;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.GameRenderer;
 import net.minecraft.client.renderer.PostChain;
+import net.minecraft.client.renderer.ShaderInstance;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.server.packs.resources.ResourceProvider;
 import net.minecraft.world.entity.Entity;
+import net.zetalasis.client.shader.RCoreShader;
+import net.zetalasis.client.shader.RPostShaderRegistry;
+import net.zetalasis.client.shader.callback.RenderCallbackRegistry;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -21,6 +27,9 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import javax.annotation.Nullable;
+import java.io.IOException;
+import java.util.Map;
+import java.util.Objects;
 
 @Mixin(GameRenderer.class)
 public class ShaderGameRenderer implements IShaderGameRenderer {
@@ -47,8 +56,62 @@ public class ShaderGameRenderer implements IShaderGameRenderer {
         return (float)roundabout$frameCount;
     }
 
+
+    @Inject(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/renderer/LevelRenderer;doEntityOutline()V", shift = At.Shift.AFTER))
+    private void roundabout$renderShaders(float tickDelta, long $$1, boolean renderLevel, CallbackInfo ci)
+    {
+        RenderCallbackRegistry.roundabout$GAME_RENDERER_FINISH(tickDelta);
+
+        if (RPostShaderRegistry.DESATURATE != null) {
+            if (((IShaderGameRenderer)Minecraft.getInstance().gameRenderer).roundabout$tsShaderStatus())
+            {
+                RPostShaderRegistry.DESATURATE.roundabout$setUniform("InvProjMat", RPostShaderRegistry.InverseProjectionMatrix);
+                RPostShaderRegistry.DESATURATE.roundabout$process(tickDelta);
+            }
+        }
+    }
+
+    @Inject(method = "reloadShaders", at=@At("HEAD"))
+    private void roundabout$reloadShaders(ResourceProvider provider, CallbackInfo ci)
+    {
+        RPostShaderRegistry.bootstrap();
+
+        try {
+            RCoreShader.roundabout$meltDodgeProgram = Objects.requireNonNull(roundabout$registerShader(provider, "meltdodge")).getProgram();
+            RCoreShader.roundabout$loveTrainProgram = Objects.requireNonNull(roundabout$registerShader(provider, "lovetrainlines")).getProgram();
+        } catch(Exception e) {
+            //put shader debug stuff here
+            Roundabout.LOGGER.info("Oops, something went wrong loading shaders");
+        }
+        Roundabout.LOGGER.info("Reloaded shaders!");
+    }
+
+    @Unique private RCoreShader roundabout$registerShader(ResourceProvider provider, String name)
+    {
+        try
+        {
+            RCoreShader shader = new RCoreShader(provider, name);
+
+            if (shader.getProgram() != null)
+            {
+                shader.getProgram().setSampler("DiffuseSampler", this.minecraft.getMainRenderTarget().getColorTextureId());
+            }
+            else {
+                throw new IOException("Shader was null!");
+            }
+
+            return shader;
+        }
+        catch (IOException e)
+        {
+            Roundabout.LOGGER.warn("roundabout${} failed to load!", name);
+            return null;
+        }
+    }
+
     @Inject(method = "renderLevel", at = @At(value = "HEAD"), cancellable = true)
     private void roundabout$renderLevel(float $$0, long $$1, PoseStack $$2, CallbackInfo ci) {
+        // TODO: copy the old depth buffer to a texture
         roundabout$frameCount++;
     }
 
@@ -116,5 +179,9 @@ public class ShaderGameRenderer implements IShaderGameRenderer {
     @Final
     Minecraft minecraft;
 
+    @Shadow @Final private Map<String, ShaderInstance> shaders;
+    @Shadow @Final private Minecraft minecraft;
+
+    @Shadow public abstract Minecraft getMinecraft();
 
 }
