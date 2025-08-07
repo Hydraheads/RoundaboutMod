72fde6ba785651cf903d3bde64d509300b7a1645
Hydraheads <Hydraheads42@gmail.com>
2025-08-07 14:06:22 -0500

    gravity direction mixin

--- common/src/main/java/net/hydra/jojomod/mixin/gravity/GravityDirectionMixin.java
+++ common/src/main/java/net/hydra/jojomod/mixin/gravity/GravityDirectionMixin.java
@@ -5,20 +5,15 @@ import net.hydra.jojomod.util.gravity.RotationUtil;
 import net.minecraft.core.Direction;
 
 import net.minecraft.world.entity.Entity;
-import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(value = Direction.class, priority = 1001)
 public abstract class GravityDirectionMixin {
-    @Shadow @Final public static Direction NORTH;
-    @Shadow @Final public static Direction SOUTH;
-    @Shadow @Final public static Direction UP;
-    @Shadow @Final public static Direction DOWN;
-    @Shadow @Final public static Direction WEST;
-    @Shadow @Final public static Direction EAST;
 
     @ModifyVariable(method = "orderedByNearest", at = @At(value = "STORE"), ordinal = 1
             )
@@ -40,6 +35,41 @@ public abstract class GravityDirectionMixin {
         return f1;
     }
 
+
+    @Inject(method = "getFacingAxis", at = @At(value = "HEAD"), cancellable = true
+    )
+    private static void roundabout$getFacingAxis(Entity entity, Direction.Axis $$1, CallbackInfoReturnable<Direction> cir) {
+        Direction gravityDirection = GravityAPI.getGravityDirection(entity);
+        if (gravityDirection != Direction.DOWN){
+            switch ($$1) {
+                case X -> {
+                    if (Direction.EAST.isFacingAngle(
+                            RotationUtil.rotPlayerToWorld(entity.getViewYRot(1.0F), entity.getViewXRot(1f), gravityDirection).x
+                    ))
+                        cir.setReturnValue(Direction.EAST);
+                    else
+                        cir.setReturnValue(Direction.WEST);
+                }
+                case Z -> {
+                    if (Direction.SOUTH.isFacingAngle(
+                            RotationUtil.rotPlayerToWorld(entity.getViewYRot(1.0F), entity.getViewXRot(1f), gravityDirection).x
+                    ))
+                        cir.setReturnValue(Direction.SOUTH);
+                    else
+                        cir.setReturnValue(Direction.NORTH);
+                }
+                case Y -> {
+                    if (
+                            RotationUtil.rotPlayerToWorld(entity.getViewYRot(1f), entity.getViewXRot(1.0F), gravityDirection).y
+                                    < 0.0F)
+                        cir.setReturnValue(Direction.UP);
+                    else
+                        cir.setReturnValue(Direction.DOWN);
+                }
+            };
+
+        }
+    }
 
     @Shadow
     private static Direction[] makeDirectionArray(Direction direction, Direction direction2, Direction direction3) {
