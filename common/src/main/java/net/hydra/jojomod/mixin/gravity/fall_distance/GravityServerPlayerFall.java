a1bcf9ef1ffd1847ce18e1b3f1ff48a8a30a1560
Hydraheads <Hydraheads42@gmail.com>
2025-08-14 14:19:59 -0500

    fixed gravity fall damage bug

--- common/src/main/java/net/hydra/jojomod/mixin/gravity/fall_distance/GravityServerPlayerFall.java
+++ common/src/main/java/net/hydra/jojomod/mixin/gravity/fall_distance/GravityServerPlayerFall.java
@@ -1,18 +1,21 @@
 package net.hydra.jojomod.mixin.gravity.fall_distance;
 
 import com.mojang.authlib.GameProfile;
+import net.hydra.jojomod.Roundabout;
 import net.hydra.jojomod.util.gravity.GravityAPI;
 import net.hydra.jojomod.util.gravity.RotationUtil;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.phys.Vec3;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(ServerPlayer.class)
@@ -36,6 +39,7 @@ public abstract class GravityServerPlayerFall extends Player {
     private void rdbt$wrapCheckFallDamage(
             double $$0, double $$1, double $$2, boolean $$3, CallbackInfo ci
     ) {
+        Roundabout.LOGGER.info("og = "+$$3+" fd ="+this.fallDistance);
         ServerPlayer this_ = (ServerPlayer) (Object) this;
         Direction gravityDirection = GravityAPI.getGravityDirection(this_);
         if (gravityDirection == Direction.DOWN) return;
