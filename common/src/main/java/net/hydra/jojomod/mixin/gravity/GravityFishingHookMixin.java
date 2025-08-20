6bdb150511f5ee985f83f96fa9d1ae2b73c62249
Hydraheads <Hydraheads42@gmail.com>
2025-08-20 18:40:42 -0500

    more work on gravity mixins

---
+++ common/src/main/java/net/hydra/jojomod/mixin/gravity/GravityFishingHookMixin.java
@@ -0,0 +1,8 @@
+package net.hydra.jojomod.mixin.gravity;
+
+import net.minecraft.world.entity.projectile.FishingHook;
+import org.spongepowered.asm.mixin.Mixin;
+
+@Mixin(FishingHook.class)
+public class GravityFishingHookMixin {
+}
---
+++ common/src/main/java/net/hydra/jojomod/mixin/gravity/GravityLlamaSpitMixin.java
@@ -0,0 +1,8 @@
+package net.hydra.jojomod.mixin.gravity;
+
+import net.minecraft.world.entity.projectile.LlamaSpit;
+import org.spongepowered.asm.mixin.Mixin;
+
+@Mixin(LlamaSpit.class)
+public class GravityLlamaSpitMixin {
+}
