5521ecb6847a990acc1e7cefc1c60c8e687daeb5
Hydraheads <Hydraheads42@gmail.com>
2025-08-20 22:26:01 -0500

    more work on gravity mixins

---
+++ common/src/main/java/net/hydra/jojomod/mixin/gravity/ThrowablePotionItemMixin.java
@@ -0,0 +1,8 @@
+package net.hydra.jojomod.mixin.gravity;
+
+import net.minecraft.world.item.ThrowablePotionItem;
+import org.spongepowered.asm.mixin.Mixin;
+
+@Mixin(ThrowablePotionItem.class)
+public class ThrowablePotionItemMixin {
+}
