84e131a063b91c41e31f832b829ab6f78b5f8490
Hydraheads <Hydraheads42@gmail.com>
2025-08-23 22:35:30 -0500

    achtung respects obsidian

---
+++ common/src/main/java/net/hydra/jojomod/mixin/achtung/AchtungPortalShapeMixin.java
@@ -0,0 +1,14 @@
+package net.hydra.jojomod.mixin.achtung;
+
+import net.hydra.jojomod.block.ModBlocks;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.portal.PortalShape;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(PortalShape.class)
+public class AchtungPortalShapeMixin {
+
+}
