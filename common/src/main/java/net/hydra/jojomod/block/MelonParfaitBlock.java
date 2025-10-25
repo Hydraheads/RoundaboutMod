91724abcb369aa860a5e15639b99f1323dbc64d9
Validfish <Jdmann5000@gmail.com>
2025-10-25 20:33:57 +0100

    you can now eat melon parfait

--- common/src/main/java/net/hydra/jojomod/block/MelonParfaitBlock.java
+++ common/src/main/java/net/hydra/jojomod/block/MelonParfaitBlock.java
@@ -1,18 +1,38 @@
 package net.hydra.jojomod.block;
 
+import net.hydra.jojomod.event.powers.StandPowers;
+import net.hydra.jojomod.event.powers.StandUser;
 import net.minecraft.core.BlockPos;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.item.ItemEntity;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.BottleItem;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.RenderShape;
+import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.StateDefinition;
+import net.minecraft.world.level.block.state.properties.IntegerProperty;
+import net.minecraft.world.phys.BlockHitResult;
 import net.minecraft.world.phys.shapes.CollisionContext;
 import net.minecraft.world.phys.shapes.VoxelShape;
+import org.jetbrains.annotations.Nullable;
 
 import javax.swing.text.html.BlockView;
 
 public class MelonParfaitBlock extends Block {
-    public MelonParfaitBlock(Properties $$0) {
-        super($$0);
+    public static final IntegerProperty EATING_STAGE = ModBlocks.EATING_STAGE;
+
+    public MelonParfaitBlock(BlockBehaviour.Properties properties) {
+        super(properties);
+        this.registerDefaultState((this.stateDefinition.any()).setValue(EATING_STAGE, 1));
     }
 
     protected static final VoxelShape SHAPE = Block.box(6.0, 0.0, 6.0, 10.0, 10.0, 10.0);
@@ -27,5 +47,44 @@ public class MelonParfaitBlock extends Block {
         return RenderShape.MODEL;
     }
 
+    @Override
+    @Nullable
+    public BlockState getStateForPlacement(BlockPlaceContext blockPlaceContext) {
+        BlockState blockState = blockPlaceContext.getLevel().getBlockState(blockPlaceContext.getClickedPos());
+        if (blockState.is(this)) {
+            int i = blockState.getValue(EATING_STAGE);
+            return (BlockState)blockState.setValue(EATING_STAGE,Math.min(3, i + 1));
+        }
+        return super.getStateForPlacement(blockPlaceContext);
+    }
 
+    @Override
+    protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
+        builder.add(EATING_STAGE);
+    }
+
+    @Override
+    public InteractionResult use(BlockState state, Level level, BlockPos pos, Player player, InteractionHand $$4, BlockHitResult $$5) {
+        int eating_stage = state.getValue(EATING_STAGE);
+        StandPowers powers = ((StandUser)  player ).roundabout$getStandPowers();
+        if (!powers.isClient()) {
+            level = powers.getSelf().level();
+            if (eating_stage < 3) {
+                if (player.getFoodData().getFoodLevel()<16) {
+                    player.getFoodData().setFoodLevel((player.getFoodData().getFoodLevel() + 5));
+                }
+                else{
+                    player.getFoodData().setFoodLevel(20);
+                }
+                level.setBlock(pos, (BlockState)state.setValue(EATING_STAGE, eating_stage + 1), 3);
+
+            } else {
+                ItemEntity glass_bottle = new ItemEntity(level,pos.getX(), pos.getY(), pos.getZ(), new ItemStack(Items.GLASS_BOTTLE));
+                level.addFreshEntity(glass_bottle);
+                BlockState air = Blocks.AIR.defaultBlockState();
+                level.setBlock(pos,air,1);
+            }
+        }
+        return  InteractionResult.SUCCESS;
+    }
 }
--- common/src/main/java/net/hydra/jojomod/block/ModBlocks.java
+++ common/src/main/java/net/hydra/jojomod/block/ModBlocks.java
@@ -30,6 +30,7 @@ public class ModBlocks {
     public static final BooleanProperty IN_FOG = BooleanProperty.create("in_fog");
     public static final IntegerProperty COLOR = IntegerProperty.create("color", 0, 12);
     public static final IntegerProperty FLESH_LAYER = IntegerProperty.create("layers", 1, 4);
+    public static final IntegerProperty EATING_STAGE = IntegerProperty.create("stage",1,3);
     public static Block ANCIENT_METEOR;
     public static Block METEOR_BLOCK;
     public static Block IMPACT_MOUND;
