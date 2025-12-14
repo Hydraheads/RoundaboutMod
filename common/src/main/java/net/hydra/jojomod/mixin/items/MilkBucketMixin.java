44cb3e4a7ac800b5aab00e2ca74f519c55250334
nikol <nikolai.y.yulaev@gmail.com>
2025-12-14 14:21:26 -0800

    milk buckets now clear 'special effects'

---
+++ common/src/main/java/net/hydra/jojomod/mixin/items/MilkBucketMixin.java
@@ -0,0 +1,39 @@
+package net.hydra.jojomod.mixin.items;
+
+import net.hydra.jojomod.util.MainUtil;
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.item.MilkBucketItem;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+import java.util.*;
+
+@Mixin(MilkBucketItem.class)
+public abstract class MilkBucketMixin {
+
+    @Redirect(method = "finishUsingItem",at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/LivingEntity;removeAllEffects()Z"))
+    public boolean roundabout$milkBucketClear(LivingEntity instance) {
+
+        List<MobEffectInstance> effects = new ArrayList<>();
+        for (MobEffectInstance effect : instance.getActiveEffects()) {
+            if (MainUtil.isSpecialEffect(effect)) {
+                effects.add(effect);
+            }
+        }
+
+
+
+        boolean bl = instance.removeAllEffects();
+
+
+        for (MobEffectInstance effect : effects) {
+            instance.addEffect(effect);
+        }
+
+        return bl;
+    }
+
+
+}
