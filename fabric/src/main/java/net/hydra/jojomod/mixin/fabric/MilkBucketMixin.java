1239edc0abc14cf317f31f4811c80772408588b0
nikol <nikolai.y.yulaev@gmail.com>
2025-12-14 14:54:34 -0800

    milk clearing mod incomp fix

--- fabric/src/main/java/net/hydra/jojomod/mixin/fabric/MilkBucketMixin.java
+++ fabric/src/main/java/net/hydra/jojomod/mixin/fabric/MilkBucketMixin.java
@@ -1,38 +1,43 @@
 package net.hydra.jojomod.mixin.fabric;
 
+import net.hydra.jojomod.Roundabout;
 import net.hydra.jojomod.util.MainUtil;
 import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.MilkBucketItem;
+import net.minecraft.world.level.Level;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import java.util.*;
 
 @Mixin(MilkBucketItem.class)
 public abstract class MilkBucketMixin {
 
-    @Redirect(method = "finishUsingItem",at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/LivingEntity;removeAllEffects()Z"))
-    public boolean roundabout$milkBucketClear(LivingEntity instance) {
+    @Unique
+    List<MobEffectInstance> effects = new ArrayList<>();
 
-        List<MobEffectInstance> effects = new ArrayList<>();
-        for (MobEffectInstance effect : instance.getActiveEffects()) {
+    @Inject(method = "finishUsingItem",at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/LivingEntity;removeAllEffects()Z"))
+    public void roundabout$beforeMilkClear(ItemStack itemStack, Level level, LivingEntity livingEntity, CallbackInfoReturnable<ItemStack> cir) {
+        for (MobEffectInstance effect : livingEntity.getActiveEffects()) {
             if (MainUtil.isSpecialEffect(effect)) {
                 effects.add(effect);
             }
         }
+    }
 
-
-
-        boolean bl = instance.removeAllEffects();
-
-
+    @Inject(method = "finishUsingItem",at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/LivingEntity;removeAllEffects()Z",shift = At.Shift.AFTER))
+    public void roundabout$afterMilkClear(ItemStack itemStack, Level level, LivingEntity livingEntity, CallbackInfoReturnable<ItemStack> cir) {
         for (MobEffectInstance effect : effects) {
-            instance.addEffect(effect);
+            livingEntity.addEffect(effect);
         }
+        effects = new ArrayList<>();
 
-        return bl;
     }
 
 
