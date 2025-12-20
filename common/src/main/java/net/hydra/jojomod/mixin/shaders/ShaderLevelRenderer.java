5cb19403e80cf2ab60c6c52dbf95570319290cdd
GoldenGate9 <g01den.g4te.9@gmail.com>
2025-12-20 09:36:19 -0700

    Timestop Shader Impl

--- common/src/main/java/net/hydra/jojomod/mixin/shaders/ShaderLevelRenderer.java
+++ common/src/main/java/net/hydra/jojomod/mixin/shaders/ShaderLevelRenderer.java
@@ -1,11 +1,17 @@
 package net.hydra.jojomod.mixin.shaders;
 
+import com.google.common.collect.ImmutableList;
 import com.mojang.blaze3d.vertex.PoseStack;
+import net.hydra.jojomod.event.TimeStopInstance;
+import net.hydra.jojomod.event.powers.TimeStop;
+import net.hydra.jojomod.util.config.ClientConfig;
+import net.hydra.jojomod.util.config.ConfigManager;
 import net.minecraft.client.Camera;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.GameRenderer;
 import net.minecraft.client.renderer.LevelRenderer;
 import net.minecraft.client.renderer.LightTexture;
+import net.minecraft.world.phys.Vec3;
 import net.zetalasis.client.shader.TimestopShaderManager;
 import org.joml.Matrix4f;
 import org.spongepowered.asm.mixin.Mixin;
@@ -23,6 +29,24 @@ public class ShaderLevelRenderer {
 
         if (TimestopShaderManager.TIMESTOP_DEPTH_BUFFER != null)
             TimestopShaderManager.TIMESTOP_DEPTH_BUFFER.copyDepthFrom(Minecraft.getInstance().getMainRenderTarget());
-        //TimestopShaderManager.renderAll(partialTick);
+
+        ClientConfig clientConfig = ConfigManager.getClientConfig();
+        if (clientConfig != null && clientConfig.timeStopSettings != null) {
+            if (ConfigManager.getClientConfig().timeStopSettings.advancedTimeStopShader) {
+                ImmutableList<TimeStopInstance> listTs = ImmutableList.copyOf(((TimeStop) Minecraft.getInstance().player.level()).rdbt$getTimeStoppingEntitiesClient());
+                if (!listTs.isEmpty()) {
+                    for (int i = listTs.size() - 1; i >= 0; --i) {
+                        TimeStopInstance tinstance = listTs.get(i);
+                        if (tinstance != null) {
+                            TimestopShaderManager.renderBubble(new TimestopShaderManager.Bubble(
+                                    new Vec3(tinstance.x, tinstance.y, tinstance.z),
+                                    11,
+                                    new Vec3(1., 1., 1.))
+                            );
+                        }
+                    }
+                }
+            }
+        }
     }
 }
