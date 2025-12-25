071faa5327e10ae270a6fc55a2ce279222e3bc10
Hydraheads <Hydraheads42@gmail.com>
2025-12-25 11:26:43 -0600

    fine tuning

--- common/src/main/java/net/hydra/jojomod/fates/powers/VampiricFate.java
+++ common/src/main/java/net/hydra/jojomod/fates/powers/VampiricFate.java
@@ -690,7 +690,7 @@ public int speedActivated = 0;
                             bloodSuckingTarget.getEyePosition().y,
                             bloodSuckingTarget.getEyePosition().z,
                              15, 0.2, 0.2, 0.2, 0.0);
-                    addBloodExp(20,bloodSuckingTarget);
+                    addBloodExp(15,bloodSuckingTarget);
                 } else {
                     self.level().playSound(null, self.getX(), self.getY(), self.getZ(), ModSounds.BLOOD_SUCK_DRAIN_EVENT, SoundSource.PLAYERS, 1F, 1.4F+(float)(Math.random()*0.1));
                     pl.getFoodData().eat(2, 0.0F);
