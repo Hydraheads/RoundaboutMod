ff0aae21433813e4dbe898804493f320c07561a6
Hydraheads <Hydraheads42@gmail.com>
2026-01-12 15:51:20 -0600

    body spikes part 3

--- common/src/main/java/net/hydra/jojomod/client/models/visages/parts/BodySpikePart.java
+++ common/src/main/java/net/hydra/jojomod/client/models/visages/parts/BodySpikePart.java
@@ -35,6 +35,7 @@ public class BodySpikePart extends PsuedoHierarchicalModel {
     private final ModelPart spike7;
     private final ModelPart spike8;
     private final ModelPart spike9;
+    private final ModelPart spike10;
 
     public BodySpikePart() {
         super(RenderType::entityTranslucent);
@@ -50,6 +51,7 @@ public class BodySpikePart extends PsuedoHierarchicalModel {
         this.spike7 = this.body_spikes.getChild("spike7");
         this.spike8 = this.body_spikes.getChild("spike8");
         this.spike9 = this.body_spikes.getChild("spike9");
+        this.spike10 = this.body_spikes.getChild("spike10");
     }
 
     public static LayerDefinition createBodyLayer() {
@@ -58,23 +60,25 @@ public class BodySpikePart extends PsuedoHierarchicalModel {
 
         PartDefinition body_spikes = partdefinition.addOrReplaceChild("body_spikes", CubeListBuilder.create(), PartPose.offset(2.5922F, 3.8399F, 0.2773F));
 
-        PartDefinition spike1 = body_spikes.addOrReplaceChild("spike1", CubeListBuilder.create().texOffs(0, 0).addBox(-0.9686F, -0.9444F, -2.0558F, 2.0F, 2.0F, 2.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(-5.2172F, 6.8564F, -1.653F, 0.3289F, 0.3736F, 0.1239F));
+        PartDefinition spike1 = body_spikes.addOrReplaceChild("spike1", CubeListBuilder.create().texOffs(0, 0).addBox(-0.996F, -0.9218F, -1.9897F, 2.0F, 2.0F, 2.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(-5.2172F, 6.8564F, -1.728F, 0.3289F, 0.3736F, 0.1239F));
 
-        PartDefinition spike2 = body_spikes.addOrReplaceChild("spike2", CubeListBuilder.create().texOffs(0, 0).addBox(-1.0622F, -1.0221F, -2.13F, 2.0F, 2.0F, 2.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(-0.4292F, 6.964F, -1.7355F, 0.3289F, -0.3736F, -0.1239F));
+        PartDefinition spike2 = body_spikes.addOrReplaceChild("spike2", CubeListBuilder.create().texOffs(0, 0).addBox(-1.0074F, -0.977F, -1.9978F, 2.0F, 2.0F, 2.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(-0.4292F, 6.964F, -1.8855F, 0.3289F, -0.3736F, -0.1239F));
 
-        PartDefinition spike3 = body_spikes.addOrReplaceChild("spike3", CubeListBuilder.create().texOffs(0, 0).addBox(-1.0071F, -1.0065F, -2.2303F, 2.0F, 2.0F, 2.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(-0.309F, 5.1574F, -0.3839F, -3.0803F, -0.9083F, 3.0453F));
+        PartDefinition spike3 = body_spikes.addOrReplaceChild("spike3", CubeListBuilder.create().texOffs(0, 0).addBox(-1.0532F, -1.0071F, -2.0045F, 2.0F, 2.0F, 2.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(-0.159F, 5.1574F, -0.2089F, -3.0803F, -0.9083F, 3.0453F));
 
-        PartDefinition spike4 = body_spikes.addOrReplaceChild("spike4", CubeListBuilder.create().texOffs(0, 0).addBox(-0.9153F, -0.9819F, -1.8835F, 2.0F, 2.0F, 2.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(-4.4882F, 5.1916F, -0.3204F, -3.0803F, 0.9083F, -3.0453F));
+        PartDefinition spike4 = body_spikes.addOrReplaceChild("spike4", CubeListBuilder.create().texOffs(0, 0).addBox(-1.0533F, -0.9885F, -1.991F, 2.0F, 2.0F, 2.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(-4.4882F, 5.1916F, -0.4954F, -3.0803F, 0.9083F, -3.0453F));
 
-        PartDefinition spike5 = body_spikes.addOrReplaceChild("spike5", CubeListBuilder.create().texOffs(0, 0).addBox(-1.0F, -0.9651F, -2.0983F, 2.0F, 2.0F, 2.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(-2.0922F, 0.668F, -1.135F, 2.2631F, 0.0F, -3.1416F));
+        PartDefinition spike5 = body_spikes.addOrReplaceChild("spike5", CubeListBuilder.create().texOffs(0, 0).addBox(-1.0F, -1.0999F, -1.9866F, 2.0F, 2.0F, 2.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(-2.0922F, 0.668F, -0.96F, 2.2631F, 0.0F, -3.1416F));
 
         PartDefinition spike6 = body_spikes.addOrReplaceChild("spike6", CubeListBuilder.create().texOffs(0, 0).addBox(-0.993F, -1.1116F, -1.9912F, 2.0F, 2.0F, 2.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(0.125F, -0.075F, 0.15F, 1.0749F, -1.2921F, -1.5112F));
 
-        PartDefinition spike7 = body_spikes.addOrReplaceChild("spike7", CubeListBuilder.create().texOffs(0, 0).addBox(-1.1521F, -1.0541F, -2.0168F, 2.0F, 2.0F, 2.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(-5.2758F, -0.3496F, 0.9614F, 1.0749F, 1.2921F, 1.5112F));
+        PartDefinition spike7 = body_spikes.addOrReplaceChild("spike7", CubeListBuilder.create().texOffs(0, 0).addBox(-1.1516F, -1.0647F, -1.9942F, 2.0F, 2.0F, 2.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(-5.3008F, -0.3496F, 0.9614F, 1.0749F, 1.2921F, 1.5112F));
 
-        PartDefinition spike8 = body_spikes.addOrReplaceChild("spike8", CubeListBuilder.create().texOffs(0, 0).addBox(-1.0388F, -0.9874F, -2.0432F, 2.0F, 2.0F, 2.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(-1.6513F, 2.3889F, -1.9627F, -0.2795F, 0.0792F, 0.4052F));
+        PartDefinition spike8 = body_spikes.addOrReplaceChild("spike8", CubeListBuilder.create().texOffs(0, 0).addBox(-1.0427F, -1.0012F, -1.9953F, 2.0F, 2.0F, 2.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(-1.6513F, 2.3889F, -2.0127F, -0.2795F, 0.0792F, 0.4052F));
 
         PartDefinition spike9 = body_spikes.addOrReplaceChild("spike9", CubeListBuilder.create().texOffs(0, 0).addBox(-0.972F, -0.9713F, -2.0005F, 2.0F, 2.0F, 2.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(-1.7352F, 2.1688F, 0.6415F, -2.8591F, -0.1631F, -2.7607F));
+
+        PartDefinition spike10 = body_spikes.addOrReplaceChild("spike10", CubeListBuilder.create().texOffs(0, 0).addBox(-1.0427F, -1.0012F, -1.9953F, 2.0F, 2.0F, 2.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(-1.6513F, 4.3889F, -2.0127F, 0.4186F, 0.0792F, 0.4052F));
 
         return LayerDefinition.create(meshdefinition, 32, 32);
     }
@@ -128,6 +132,7 @@ public class BodySpikePart extends PsuedoHierarchicalModel {
                 } else {
                     partial = Math.min(gp.spikeTimeDuring+partial,VampireGeneralPowers.maxSpike);
                 }
+
                 modifySpike(spike1,partial,tier2);
                 modifySpike(spike2,partial,tier2);
                 modifySpike(spike3,partial,tier2);
@@ -137,22 +142,46 @@ public class BodySpikePart extends PsuedoHierarchicalModel {
                 modifySpike(spike7,partial,tier2);
                 modifySpike(spike8,partial,tier2);
                 modifySpike(spike9,partial,tier2);
+                modifySpike(spike10,partial,tier2);
+                root().render(poseStack, consumer, light, OverlayTexture.NO_OVERLAY, r, g, b, alpha);
+
+                modifySpike2(spike1,partial,tier2);
+                modifySpike2(spike2,partial,tier2);
+                modifySpike2(spike3,partial,tier2);
+                modifySpike2(spike4,partial,tier2);
+                modifySpike2(spike5,partial,tier2);
+                modifySpike2(spike6,partial,tier2);
+                modifySpike2(spike7,partial,tier2);
+                modifySpike2(spike8,partial,tier2);
+                modifySpike2(spike9,partial,tier2);
+                modifySpike2(spike10,partial,tier2);
+                root().render(poseStack, consumer, light, OverlayTexture.NO_OVERLAY, r, g, b, alpha);
             }
 
-            root().render(poseStack, consumer, light, OverlayTexture.NO_OVERLAY, r, g, b, alpha);
         }
     }
     public static float maxSize2 = 48;
 
     public void modifySpike(ModelPart spike, float partial, boolean tier2){
         if (spike != null){
-                spike.xScale = 0.4F;
-                spike.yScale = 0.4F;
+                spike.xScale = 0.01F;
+                spike.yScale = 0.5F;
                 if (!tier2){
                     spike.zScale = (partial)/3;
                 } else {
                     spike.zScale = partial;
                 }
+        }
+    }
+    public void modifySpike2(ModelPart spike, float partial, boolean tier2){
+        if (spike != null){
+            spike.xScale = 0.5F;
+            spike.yScale = 0.01F;
+            if (!tier2){
+                spike.zScale = (partial)/3;
+            } else {
+                spike.zScale = partial;
+            }
         }
     }
 
