2102b6c62239226b44f83c9b111a921e51f29169
nikol <nikolai.y.yulaev@gmail.com>
2026-01-18 19:58:13 -0800

    anubis bonus xp scales with level

--- common/src/main/java/net/hydra/jojomod/mixin/anubis/BonusExperienceMixin.java
+++ common/src/main/java/net/hydra/jojomod/mixin/anubis/BonusExperienceMixin.java
@@ -1,6 +1,8 @@
 package net.hydra.jojomod.mixin.anubis;
 
+import net.hydra.jojomod.access.IPlayerEntity;
 import net.hydra.jojomod.event.powers.StandUser;
+import net.hydra.jojomod.item.MaxStandDiscItem;
 import net.hydra.jojomod.stand.powers.PowersAnubis;
 import net.minecraft.world.entity.ExperienceOrb;
 import net.minecraft.world.entity.player.Player;
@@ -15,8 +17,9 @@ public abstract class BonusExperienceMixin {
     private void roundabout$addBonusXp(Player instance, int i) {
         StandUser SU = (StandUser) instance;
         float extra = 1F;
-        if (SU.roundabout$getStandPowers() instanceof PowersAnubis ) {
-            extra = 1.5F;
+        if (SU.roundabout$getStandPowers() instanceof PowersAnubis PA ) {
+            boolean bypass = (SU.roundabout$getStandDisc().getItem() instanceof MaxStandDiscItem) || instance.isCreative();
+            extra = 1 + (bypass ? 5F : 0.5F * ((IPlayerEntity) instance).roundabout$getStandLevel() / PA.getMaxLevel()  ) ;
         }
         instance.giveExperiencePoints((int) (i*extra) );
 
