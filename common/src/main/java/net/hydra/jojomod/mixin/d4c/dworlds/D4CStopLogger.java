3be2576cb2c6a09bcc01488e75f01c6ee7e85324
Hydraheads <Hydraheads42@gmail.com>
2026-02-27 14:51:13 -0600

    removed redirect mixin

--- common/src/main/java/net/hydra/jojomod/mixin/d4c/dworlds/D4CStopLogger.java
+++ common/src/main/java/net/hydra/jojomod/mixin/d4c/dworlds/D4CStopLogger.java
@@ -9,19 +9,4 @@ import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(MinecraftServer.class)
 public class D4CStopLogger {
-    @Redirect(method = "saveAllChunks", at = @At(value = "INVOKE", target = "Lorg/slf4j/Logger;info(Ljava/lang/String;Ljava/lang/Object;Ljava/lang/Object;)V"), require = 1)
-    private void roundabout$stopLoggingD4CWorlds(Logger instance, String message, Object arg1, Object arg2)
-    {
-        if (arg2 instanceof ResourceLocation location && !location.toString().startsWith("roundabout:d4c-")) {
-            instance.info(message, arg1, arg2);
-        }
-    }
-
-    @Redirect(method = "saveAllChunks", at = @At(value = "INVOKE", target = "Lorg/slf4j/Logger;info(Ljava/lang/String;Ljava/lang/Object;)V"), require = 1)
-    private void roundabout$stopLoggingD4CChunks(Logger instance, String message, Object arg1)
-    {
-        if (arg1 instanceof ResourceLocation location && !location.toString().startsWith("roundabout:d4c-")) {
-            instance.info(message, arg1);
-        }
-    }
 }
