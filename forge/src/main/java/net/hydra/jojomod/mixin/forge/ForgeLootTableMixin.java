1606afa5af10eca40b08feca168af2123811974e
Hydraheads <Hydraheads42@gmail.com>
2026-02-27 14:21:47 -0600

    fixed forge crash from loot table code change

--- forge/src/main/java/net/hydra/jojomod/mixin/forge/ForgeLootTableMixin.java
+++ forge/src/main/java/net/hydra/jojomod/mixin/forge/ForgeLootTableMixin.java
@@ -1,4 +1,4 @@
-package net.hydra.jojomod.mixin.data_and_structures;
+package net.hydra.jojomod.mixin.forge;
 
 import net.hydra.jojomod.access.ILootPool;
 import net.hydra.jojomod.access.ILootTable;
@@ -8,13 +8,13 @@ import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 
+import java.util.List;
+
 @Mixin(LootTable.class)
-public abstract class LootTableMixin implements ILootTable {
+public abstract class ForgeLootTableMixin implements ILootTable {
 
-
-    @Shadow
-    @Final
-    public LootPool[] pools;
+    @Shadow @Final
+    private List<LootPool> pools;
 
     @Override
     public int roundabout$getSize() {
