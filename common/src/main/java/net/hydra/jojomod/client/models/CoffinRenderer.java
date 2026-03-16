a7eb917e4ec23db4d506b5aff5659f343165fb94
Hydraheads <Hydraheads42@gmail.com>
2026-03-15 23:41:26 -0500

    coffin rendering

--- common/src/main/java/net/hydra/jojomod/client/models/CoffinRenderer.java
+++ common/src/main/java/net/hydra/jojomod/client/models/CoffinRenderer.java
@@ -2,6 +2,7 @@ package net.hydra.jojomod.client.models;
 
 import com.mojang.blaze3d.vertex.PoseStack;
 import com.mojang.blaze3d.vertex.VertexConsumer;
+import com.mojang.math.Axis;
 import net.hydra.jojomod.Roundabout;
 import net.hydra.jojomod.block.CoffinBlock;
 import net.hydra.jojomod.block.ModBlocks;
@@ -37,26 +38,32 @@ public class CoffinRenderer <T extends BlockEntity> implements BlockEntityRender
     private final ModelPart right_coffin;
     private final ModelPart left_lid;
     private final ModelPart right_lid;
+    private final ModelPart left_bottom;
+    private final ModelPart right_bottom;
 
     public CoffinRenderer(BlockEntityRendererProvider.Context $$0) {
         ModelPart $$2 = $$0.bakeLayer(ModEntityRendererClient.COFFIN_LEFT_LAYER);
         this.left_coffin = $$2.getChild("coffin");
         this.left_lid = left_coffin.getChild("lid");
+        this.left_bottom = left_coffin.getChild("bottom");
         ModelPart $$3 = $$0.bakeLayer(ModEntityRendererClient.COFFIN_RIGHT_LAYER);
-        this.right_coffin = $$2.getChild("coffin");
+        this.right_coffin = $$3.getChild("coffin");
         this.right_lid = right_coffin.getChild("lid");
+        this.right_bottom = right_coffin.getChild("bottom");
     }
 
     public static LayerDefinition createDoubleBodyRightLayer() {
         MeshDefinition meshdefinition = new MeshDefinition();
         PartDefinition partdefinition = meshdefinition.getRoot();
 
-        PartDefinition coffin = partdefinition.addOrReplaceChild("coffin", CubeListBuilder.create().texOffs(96, 69).addBox(-6.0F, -1.0F, 0.0F, 12.0F, 2.0F, 14.0F, new CubeDeformation(0.0F))
+        PartDefinition coffin = partdefinition.addOrReplaceChild("coffin", CubeListBuilder.create(), PartPose.offset(0.0F, 24.0F, 0.0F));
+
+        PartDefinition lid = coffin.addOrReplaceChild("lid", CubeListBuilder.create().texOffs(96, 0).addBox(0.0F, -3.0F, 0.0F, 12.0F, 3.0F, 16.0F, new CubeDeformation(0.0F)), PartPose.offset(-6.0F, -9.0F, -8.0F));
+
+        PartDefinition bottom = coffin.addOrReplaceChild("bottom", CubeListBuilder.create().texOffs(96, 69).addBox(-6.0F, -1.0F, 0.0F, 12.0F, 2.0F, 14.0F, new CubeDeformation(0.0F))
                 .texOffs(108, 19).addBox(-8.0F, -8.0F, 0.0F, 2.0F, 9.0F, 16.0F, new CubeDeformation(0.0F))
                 .texOffs(108, 44).addBox(6.0F, -8.0F, 0.0F, 2.0F, 9.0F, 16.0F, new CubeDeformation(0.0F))
-                .texOffs(96, 19).addBox(-6.0F, -8.0F, 14.0F, 12.0F, 9.0F, 2.0F, new CubeDeformation(0.0F)), PartPose.offset(0.0F, 23.0F, 0.0F));
-
-        PartDefinition lid = coffin.addOrReplaceChild("lid", CubeListBuilder.create().texOffs(96, 0).addBox(0.0F, -3.0F, 0.0F, 12.0F, 3.0F, 16.0F, new CubeDeformation(0.0F)), PartPose.offset(-6.0F, -8.0F, 0.0F));
+                .texOffs(96, 19).addBox(-6.0F, -8.0F, 14.0F, 12.0F, 9.0F, 2.0F, new CubeDeformation(0.0F)), PartPose.offset(0.0F, -1.0F, -8.0F));
 
         return LayerDefinition.create(meshdefinition, 160, 160);
     }
@@ -65,12 +72,14 @@ public class CoffinRenderer <T extends BlockEntity> implements BlockEntityRender
         MeshDefinition meshdefinition = new MeshDefinition();
         PartDefinition partdefinition = meshdefinition.getRoot();
 
-        PartDefinition coffin = partdefinition.addOrReplaceChild("coffin", CubeListBuilder.create().texOffs(0, 69).addBox(-6.0F, -1.0F, -14.0F, 12.0F, 2.0F, 14.0F, new CubeDeformation(0.0F))
-                .texOffs(0, 19).addBox(-6.0F, -8.0F, -16.0F, 12.0F, 9.0F, 2.0F, new CubeDeformation(0.0F))
-                .texOffs(12, 19).addBox(-8.0F, -8.0F, -16.0F, 2.0F, 9.0F, 16.0F, new CubeDeformation(0.0F))
-                .texOffs(12, 44).addBox(6.0F, -8.0F, -16.0F, 2.0F, 9.0F, 16.0F, new CubeDeformation(0.0F)), PartPose.offset(0.0F, 23.0F, 0.0F));
+        PartDefinition coffin = partdefinition.addOrReplaceChild("coffin", CubeListBuilder.create(), PartPose.offset(0.0F, 24.0F, 0.0F));
 
-        PartDefinition lid = coffin.addOrReplaceChild("lid", CubeListBuilder.create().texOffs(0, 0).addBox(0.0F, -3.0F, -16.0F, 12.0F, 3.0F, 16.0F, new CubeDeformation(0.0F)), PartPose.offset(-6.0F, -8.0F, 0.0F));
+        PartDefinition lid = coffin.addOrReplaceChild("lid", CubeListBuilder.create().texOffs(0, 0).addBox(0.0F, -3.0F, -16.0F, 12.0F, 3.0F, 16.0F, new CubeDeformation(0.0F)), PartPose.offset(-6.0F, -9.0F, 8.0F));
+
+        PartDefinition bottom = coffin.addOrReplaceChild("bottom", CubeListBuilder.create().texOffs(0, 69).addBox(0.0F, 7.0F, -14.0F, 12.0F, 2.0F, 14.0F, new CubeDeformation(0.0F))
+                .texOffs(0, 19).addBox(0.0F, 0.0F, -16.0F, 12.0F, 9.0F, 2.0F, new CubeDeformation(0.0F))
+                .texOffs(12, 19).addBox(-2.0F, 0.0F, -16.0F, 2.0F, 9.0F, 16.0F, new CubeDeformation(0.0F))
+                .texOffs(12, 44).addBox(12.0F, 0.0F, -16.0F, 2.0F, 9.0F, 16.0F, new CubeDeformation(0.0F)), PartPose.offset(-6.0F, -9.0F, 8.0F));
 
         return LayerDefinition.create(meshdefinition, 160, 160);
     }
@@ -82,24 +91,30 @@ public class CoffinRenderer <T extends BlockEntity> implements BlockEntityRender
         boolean $$7 = $$6 != null;
         BlockState $$8 = $$7 ? $$0.getBlockState() : ModBlocks.COFFIN_BLOCK.defaultBlockState().setValue(ChestBlock.FACING, Direction.SOUTH);
         if ($$8.getBlock() instanceof CoffinBlock $$11) {
-            BedPart $$9 = $$8.hasProperty(BedBlock.PART) ? $$8.getValue(BedBlock.PART) : BedPart.HEAD;
+            BedPart $$9 = $$8.hasProperty(CoffinBlock.PART) ? $$8.getValue(CoffinBlock.PART) : BedPart.HEAD;
 
 
-            VertexConsumer vertexConsumer = $$3.getBuffer(RenderType.entityCutout(COFFIN));
+            float $$13 = $$8.getValue(CoffinBlock.FACING).toYRot();
             $$2.pushPose();
+            $$2.translate(0.5F, 0.5F, 0.5F);
+            $$2.mulPose(Axis.YP.rotationDegrees(-$$13));
+            $$2.translate(-0.5F, -0.5F, -0.5F);
+            $$2.mulPose(Axis.ZP.rotationDegrees(180));
+            $$2.translate(-0.5F, 0F, 0.5F);
+            VertexConsumer vertexConsumer = $$3.getBuffer(RenderType.entityCutout(COFFIN));
 
-            float openness = 0.0f;
+            float openness = 0.4f;
             if ($$9 == BedPart.HEAD) {
-                this.render($$2, vertexConsumer, this.left_lid, this.left_coffin, openness, $$4, $$5);
+                this.render($$2, vertexConsumer, this.right_lid, this.right_bottom, openness, $$4, $$5);
             } else {
-                this.render($$2, vertexConsumer, this.right_lid, this.right_coffin, openness, $$4, $$5);
+                this.render($$2, vertexConsumer, this.left_lid, this.left_bottom, openness, $$4, $$5);
             }
             $$2.popPose();
         }
     }
 
     private void render(PoseStack $$0, VertexConsumer $$1, ModelPart $$2, ModelPart $$4, float $$5, int $$6, int $$7) {
-        $$2.xRot = -($$5 * (float) (Math.PI / 2));
+        $$2.zRot = -($$5 * (float) (Math.PI / 2));
         $$2.render($$0, $$1, $$6, $$7);
         $$4.render($$0, $$1, $$6, $$7);
     }
--- fabric/src/main/java/net/hydra/jojomod/mixin/fabric/FabricTerrainRenderContext.java
+++ fabric/src/main/java/net/hydra/jojomod/mixin/fabric/FabricTerrainRenderContext.java
@@ -1,20 +1,7 @@
 package net.hydra.jojomod.mixin.fabric;
 
-import com.mojang.blaze3d.vertex.PoseStack;
-import com.mojang.blaze3d.vertex.VertexConsumer;
 import net.fabricmc.fabric.impl.client.indigo.renderer.render.TerrainRenderContext;
-import net.hydra.jojomod.Roundabout;
-import net.hydra.jojomod.util.MainUtil;
-import net.minecraft.client.resources.model.BakedModel;
-import net.minecraft.core.BlockPos;
-import net.minecraft.util.RandomSource;
-import net.minecraft.world.level.BlockAndTintGetter;
-import net.minecraft.world.level.block.state.BlockState;
-import net.minecraft.world.phys.shapes.Shapes;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(TerrainRenderContext.class)
 
