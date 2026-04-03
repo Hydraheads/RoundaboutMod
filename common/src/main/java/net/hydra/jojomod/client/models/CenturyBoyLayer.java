c758c1ebe75131a2d6ab484c6e76e411000a533e
igpp0 <igorppenchel@gmail.com>
2026-04-02 21:35:50 -0300

    20cb - work on layer stuff

--- common/src/main/java/net/hydra/jojomod/client/models/CenturyBoyLayer.java
+++ common/src/main/java/net/hydra/jojomod/client/models/CenturyBoyLayer.java
@@ -3,6 +3,10 @@ package net.hydra.jojomod.client.models;
 import com.mojang.blaze3d.vertex.PoseStack;
 import net.hydra.jojomod.access.IEntityAndData;
 import net.hydra.jojomod.client.ClientUtil;
+import net.hydra.jojomod.event.index.PowerTypes;
+import net.hydra.jojomod.event.powers.StandUser;
+import net.hydra.jojomod.event.powers.TimeStop;
+import net.hydra.jojomod.stand.powers.Powers20thCenturyBoy;
 import net.minecraft.client.model.HumanoidModel;
 import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.entity.EntityRenderDispatcher;
@@ -22,11 +26,43 @@ public class CenturyBoyLayer<T extends LivingEntity, A extends HumanoidModel<T>>
     @Override
     public void render(PoseStack poseStack, MultiBufferSource bufferSource, int packedLight, T entity, float var5, float var6, float var7, float partialTicks, float var9, float var10) {
         if (ClientUtil.canSeeStands(ClientUtil.getPlayer())){
-            if (((IEntityAndData)entity).roundabout$getTrueInvisibility() > - 1 && !ClientUtil.checkIfClientCanSeeInvisAchtung())
-                return;
+            if (entity != null){
+                if (((IEntityAndData)entity).roundabout$getTrueInvisibility() > - 1 && !ClientUtil.checkIfClientCanSeeInvisAchtung())
+                    return;
 
-            LivingEntity LE = entity;
-            if ()
+
+                StandUser user = ((StandUser) entity);
+                boolean hasCB = (user.roundabout$getStandPowers() instanceof Powers20thCenturyBoy);
+                boolean standOut = (PowerTypes.hasStandActive(entity) && hasCB);
+                if (!entity.isInvisible()){
+                    int CBticks = user.roundabout$getCBVanishTicks();
+
+                    if (CBticks > 0 || standOut){
+                        byte skin = user.roundabout$getStandSkin();
+                        if (user.roundabout$getLastStandSkin() != skin){
+                            user.roundabout$setLastStandSkin(skin);
+                            CBticks = 0;
+                            user.roundabout$setCBVanishTicks(0);
+                        }
+
+                        float heyfull = 0;
+                        float fixedPartial = partialTicks - (int) partialTicks;
+
+                        if (((TimeStop) entity.level()).CanTimeStopEntity(entity)){
+                            fixedPartial = 0;
+                        }
+
+                        if (standOut){
+                            heyfull = CBticks + fixedPartial;
+                            heyfull = Math.min(heyfull / 10, 1f);
+                        } else {
+                            heyfull = CBticks - fixedPartial;
+                            heyfull = Math.max(heyfull / 10, 0)
+                        }
+
+                    }
+                }
+            }
         }
     }
 }
