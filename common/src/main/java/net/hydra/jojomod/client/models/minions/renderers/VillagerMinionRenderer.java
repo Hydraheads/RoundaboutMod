f2b74bc0ae1f7304f298245528b18cdfca678c95
Hydraheads <Hydraheads42@gmail.com>
2026-04-14 12:25:35 -0500

    whoops

--- common/src/main/java/net/hydra/jojomod/client/models/minions/renderers/VillagerMinionRenderer.java
+++ common/src/main/java/net/hydra/jojomod/client/models/minions/renderers/VillagerMinionRenderer.java
@@ -4,6 +4,7 @@ import com.mojang.blaze3d.vertex.PoseStack;
 import net.hydra.jojomod.Roundabout;
 import net.hydra.jojomod.client.models.layers.ModEntityRendererClient;
 import net.hydra.jojomod.client.models.minions.VillagerMinionModel;
+import net.hydra.jojomod.entity.zombie_minion.BaseMinion;
 import net.hydra.jojomod.entity.zombie_minion.VillagerMinion;
 import net.minecraft.client.model.geom.ModelLayers;
 import net.minecraft.client.renderer.MultiBufferSource;
@@ -11,9 +12,10 @@ import net.minecraft.client.renderer.entity.EntityRendererProvider;
 import net.minecraft.client.renderer.entity.MobRenderer;
 import net.minecraft.client.renderer.entity.layers.ItemInHandLayer;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.entity.ambient.Bat;
 import net.minecraft.world.entity.monster.Vindicator;
 
-public class VillagerMinionRenderer<T extends VillagerMinion> extends MobRenderer<VillagerMinion, VillagerMinionModel<VillagerMinion>> {
+public class VillagerMinionRenderer extends MobRenderer<VillagerMinion, VillagerMinionModel<VillagerMinion>> {
     private static final ResourceLocation VINDICATOR = new ResourceLocation(Roundabout.MOD_ID,"textures/entity/minions/villager.png");
 
     public VillagerMinionRenderer(EntityRendererProvider.Context $$0) {
@@ -26,11 +28,13 @@ public class VillagerMinionRenderer<T extends VillagerMinion> extends MobRendere
             }
         });
     }
-
-
-    public void scale(T $$0, PoseStack $$1, float $$2) {
-        float $$3 = 0.9375F;
+        @Override
+    protected void scale(VillagerMinion $$0, PoseStack $$1, float $$2) {
         $$1.scale(0.9375F, 0.9375F, 0.9375F);
+        if ($$0.clientDigProg > 0 && $$0.getDigProg() <= -1){
+            float perc = 1f-(($$0.clientDigProg-$$2)/((float) BaseMinion.digProgTick));
+            $$1.scale(1F, perc, 1F);
+        }
     }
     public ResourceLocation getTextureLocation(VillagerMinion $$0) {
         return VINDICATOR;
