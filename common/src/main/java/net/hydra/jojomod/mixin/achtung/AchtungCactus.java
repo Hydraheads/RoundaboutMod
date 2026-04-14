7937ee2d84cc978803c369bd02d731b46d7da5c6
Hydraheads <Hydraheads42@gmail.com>
2026-04-14 15:40:53 -0500

    achtung cactus fix

---
+++ common/src/main/java/net/hydra/jojomod/mixin/achtung/AchtungCactus.java
@@ -0,0 +1,25 @@
+package net.hydra.jojomod.mixin.achtung;
+
+import net.hydra.jojomod.block.ModBlocks;
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.LevelReader;
+import net.minecraft.world.level.block.CactusBlock;
+import net.minecraft.world.level.block.ChorusFlowerBlock;
+import net.minecraft.world.level.block.state.BlockState;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(CactusBlock.class)
+public class AchtungCactus {
+
+    /**The invisible blocks of achtung baby should not be breaking chorus plants*/
+    @Inject(method = "canSurvive(Lnet/minecraft/world/level/block/state/BlockState;Lnet/minecraft/world/level/LevelReader;Lnet/minecraft/core/BlockPos;)Z", at = @At("HEAD"), cancellable = true)
+    public void roundabout$canSurvive(BlockState $$0, LevelReader $$1, BlockPos $$2, CallbackInfoReturnable<Boolean> cir) {
+        BlockState blockstate = $$1.getBlockState($$2.below());
+        if (blockstate.is(ModBlocks.INVISIBLOCK)){
+            cir.setReturnValue(true);
+        }
+    }
+}
