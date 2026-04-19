e63f09cae4afee58445f855d691a23be125ffc72
Hydraheads <Hydraheads42@gmail.com>
2026-04-18 22:52:59 -0500

    early work on head rendering

---
+++ bbmodels/Visages and NPCs/polar_bear_hat.java
@@ -0,0 +1,38 @@
+// Made with Blockbench 5.1.1
+// Exported for Minecraft version 1.17 or later with Mojang mappings
+// Paste this class into your mod and generate all required imports
+
+
+public class goat_hat<T extends Entity> extends EntityModel<T> {
+	// This layer location should be baked with EntityRendererProvider.Context in the entity renderer and passed into this model's constructor
+	public static final ModelLayerLocation LAYER_LOCATION = new ModelLayerLocation(new ResourceLocation("modid", "goat_hat"), "main");
+	private final ModelPart head;
+
+	public goat_hat(ModelPart root) {
+		this.head = root.getChild("head");
+	}
+
+	public static LayerDefinition createBodyLayer() {
+		MeshDefinition meshdefinition = new MeshDefinition();
+		PartDefinition partdefinition = meshdefinition.getRoot();
+
+		PartDefinition head = partdefinition.addOrReplaceChild("head", CubeListBuilder.create().texOffs(2, 61).addBox(-5.5F, -7.5F, -1.5F, 3.0F, 2.0F, 1.0F, new CubeDeformation(0.0F))
+		.texOffs(12, 55).addBox(-2.475F, -12.5F, -1.0F, 2.0F, 7.0F, 2.0F, new CubeDeformation(0.0F))
+		.texOffs(12, 55).addBox(0.475F, -12.5F, -1.0F, 2.0F, 7.0F, 2.0F, new CubeDeformation(0.0F))
+		.texOffs(2, 61).mirror().addBox(2.5F, -7.5F, -1.5F, 3.0F, 2.0F, 1.0F, new CubeDeformation(0.0F)).mirror(false), PartPose.offset(0.0F, 0.0F, 0.0F));
+
+		PartDefinition cube_r1 = head.addOrReplaceChild("cube_r1", CubeListBuilder.create().texOffs(34, 46).addBox(-2.5F, -7.0F, -5.0F, 5.0F, 7.0F, 10.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(0.0F, 0.0F, 1.0F, 0.8727F, 0.0F, 0.0F));
+
+		return LayerDefinition.create(meshdefinition, 64, 64);
+	}
+
+	@Override
+	public void setupAnim(Entity entity, float limbSwing, float limbSwingAmount, float ageInTicks, float netHeadYaw, float headPitch) {
+
+	}
+
+	@Override
+	public void renderToBuffer(PoseStack poseStack, VertexConsumer vertexConsumer, int packedLight, int packedOverlay, float red, float green, float blue, float alpha) {
+		head.render(poseStack, vertexConsumer, packedLight, packedOverlay, red, green, blue, alpha);
+	}
+}
