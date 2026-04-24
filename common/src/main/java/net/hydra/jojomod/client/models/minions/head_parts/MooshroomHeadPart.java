c093f2e98a66093b8bac40f7e46a7564066139a4
Hydraheads <Hydraheads42@gmail.com>
2026-04-24 12:46:01 -0500

    mooshroom head z fighting adjustment

--- common/src/main/java/net/hydra/jojomod/client/models/minions/head_parts/MooshroomHeadPart.java
+++ common/src/main/java/net/hydra/jojomod/client/models/minions/head_parts/MooshroomHeadPart.java
@@ -37,7 +37,7 @@ public class MooshroomHeadPart extends PsuedoHierarchicalModel {
         PartDefinition partdefinition = meshdefinition.getRoot();
 
         PartDefinition head = partdefinition.addOrReplaceChild("head", CubeListBuilder.create().texOffs(0, 0).addBox(-4.0F, -8.0F, -3.0F, 8.0F, 8.0F, 6.0F, new CubeDeformation(0.0F))
-                .texOffs(0, 32).addBox(-3.0F, -3.0F, -4.0F, 6.0F, 3.0F, 2.0F, new CubeDeformation(0.0F))
+                .texOffs(0, 32).addBox(-3.0F, -3.075F, -4.0F, 6.0F, 3.0F, 2.0F, new CubeDeformation(0.0F))
                 .texOffs(22, 0).addBox(-5.0F, -9.0F, -2.0F, 1.0F, 3.0F, 1.0F, new CubeDeformation(0.0F))
                 .texOffs(22, 0).addBox(4.0F, -9.0F, -2.0F, 1.0F, 3.0F, 1.0F, new CubeDeformation(0.0F)), PartPose.offset(0.0F, 0.0F, 0.0F));
 
