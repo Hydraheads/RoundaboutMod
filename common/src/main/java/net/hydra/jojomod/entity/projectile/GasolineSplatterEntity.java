41189c828c6f9c8ef17c7e6bbe8e116a7a91dee0
Hydraheads <Hydraheads42@gmail.com>
2026-05-12 21:59:48 -0500

    war hammer crafting

--- common/src/main/java/net/hydra/jojomod/entity/projectile/GasolineSplatterEntity.java
+++ common/src/main/java/net/hydra/jojomod/entity/projectile/GasolineSplatterEntity.java
@@ -1,15 +1,9 @@
 package net.hydra.jojomod.entity.projectile;
 
-import net.hydra.jojomod.Roundabout;
-import net.hydra.jojomod.access.IFireBlock;
-import net.hydra.jojomod.access.IMinecartTNT;
-import net.hydra.jojomod.block.GasolineBlock;
 import net.hydra.jojomod.block.ModBlocks;
 import net.hydra.jojomod.block.StandFireBlock;
 import net.hydra.jojomod.entity.ModEntities;
-import net.hydra.jojomod.event.powers.ModDamageTypes;
 import net.hydra.jojomod.event.powers.StandUser;
-import net.hydra.jojomod.item.ModItems;
 import net.hydra.jojomod.util.MainUtil;
 import net.hydra.jojomod.util.gravity.GravityAPI;
 import net.hydra.jojomod.util.gravity.RotationUtil;
@@ -21,7 +15,6 @@ import net.minecraft.server.level.ServerLevel;
 import net.minecraft.sounds.SoundEvent;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.util.Mth;
-import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.LivingEntity;
@@ -29,10 +22,7 @@ import net.minecraft.world.entity.projectile.ThrowableItemProjectile;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.*;
-import net.minecraft.world.level.block.state.BlockState;
-import net.minecraft.world.level.material.Fluid;
 import net.minecraft.world.level.material.Fluids;
-import net.minecraft.world.level.material.LavaFluid;
 import net.minecraft.world.phys.BlockHitResult;
 import net.minecraft.world.phys.EntityHitResult;
 import net.minecraft.world.phys.Vec2;
@@ -58,7 +48,6 @@ public class GasolineSplatterEntity extends ThrowableItemProjectile {
         return ModBlocks.GASOLINE_SPLATTER.asItem();
     }
 
-    public boolean isBundle = false;
 
     @Override
     protected void onHitBlock(BlockHitResult $$0) {
