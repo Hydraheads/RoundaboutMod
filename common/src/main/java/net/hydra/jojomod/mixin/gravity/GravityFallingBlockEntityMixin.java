d05aef26f7e242ff14fc4b380849918bc74d120b
Hydraheads <Hydraheads42@gmail.com>
2026-05-27 14:33:22 -0500

    falling block / anvil uniform throw

--- common/src/main/java/net/hydra/jojomod/mixin/gravity/GravityFallingBlockEntityMixin.java
+++ common/src/main/java/net/hydra/jojomod/mixin/gravity/GravityFallingBlockEntityMixin.java
@@ -1,8 +1,17 @@
 package net.hydra.jojomod.mixin.gravity;
 
+import net.hydra.jojomod.access.IEntityAndData;
 import net.minecraft.world.entity.item.FallingBlockEntity;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(FallingBlockEntity.class)
 public class GravityFallingBlockEntityMixin {
+    @Inject(method = "tick", at = @At(value = "TAIL"))
+    protected void roundabout$tick(CallbackInfo ci) {
+        ((IEntityAndData)this).roundabout$universalTick();
+        ((IEntityAndData)this).roundabout$tickQVec();
+    }
 }
