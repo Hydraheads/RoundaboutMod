464d8532f8a99edaae703afefc694c0527318d5b
Kacper773539 <dlg.kacper@gmail.com>
2026-06-22 12:32:39 +0200

    Manhattan 3rd ps camera quick fix

--- common/src/main/java/net/hydra/jojomod/stand/powers/PowersManhattanTransfer.java
+++ common/src/main/java/net/hydra/jojomod/stand/powers/PowersManhattanTransfer.java
@@ -524,7 +524,7 @@ public class PowersManhattanTransfer extends NewDashPreset {
             LivingEntity ent = getPilotingStand();
             if (ent != null) {
                 if(someTicks < 1){
-                    ClientUtil.synchToCamera(ent);
+                    ClientUtil.synchToCameraAllPersons(ent);
                 }
             }
         }
