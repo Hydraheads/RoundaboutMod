29e82a4a10ad32da64893f87527446eb66c2bb20
Misael <ribeiromisa05@gmail.com>
2026-06-29 17:13:06 -0300

    added forge custom item properties and killer queen bug fixed

---
+++ forge/src/main/java/net/hydra/jojomod/registry/ForgeItemProperties.java
@@ -0,0 +1,13 @@
+package net.hydra.jojomod.registry;
+
+import net.hydra.jojomod.Roundabout;
+import net.hydra.jojomod.item.StrayCats.AbtractStrayCat;
+import net.minecraft.client.renderer.item.ItemProperties;
+import net.minecraft.resources.ResourceLocation;
+
+public class ForgeItemProperties {
+    public static void addCustomItemProperties() {
+        ItemProperties.register(ForgeItems.STRAY_CAT_ANIME.get(), new ResourceLocation(Roundabout.MOD_ID,"anim"), (itemStack, clientLevel, livingEntity, i) ->  !itemStack.isEmpty() ? ((AbtractStrayCat)(itemStack.getItem())).getCurrentPredicateValue(clientLevel) : 0.0f);
+        ItemProperties.register(ForgeItems.STRAY_CAT_MANGA.get(), new ResourceLocation(Roundabout.MOD_ID,"anim"), (itemStack, clientLevel, livingEntity, i) ->  !itemStack.isEmpty() ? ((AbtractStrayCat)(itemStack.getItem())).getCurrentPredicateValue(clientLevel) : 0.0f);
+    }
+}
