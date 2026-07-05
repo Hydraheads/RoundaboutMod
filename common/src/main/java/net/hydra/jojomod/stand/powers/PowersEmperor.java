68e1fde4459e13ea45dc01f405fa5c7f4f5c03b1
Hydraheads <Hydraheads42@gmail.com>
2026-07-05 07:57:53 -0500

    fixed crash

--- common/src/main/java/net/hydra/jojomod/stand/powers/PowersEmperor.java
+++ common/src/main/java/net/hydra/jojomod/stand/powers/PowersEmperor.java
@@ -215,7 +215,7 @@ public class PowersEmperor extends NewDashPreset {
         PA.controlMode = true;
         PA.autoMode = false;
 
-        ((StandUser)entity).roundabout$setStandSkinLight((byte) 1);
+        standSkin = 1;
         return PA;
     }
 
