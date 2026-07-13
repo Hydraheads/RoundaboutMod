f158135156b14dd5f179ed0fec0e8924bb9082ab
Hydraheads <Hydraheads42@gmail.com>
2026-07-13 11:27:55 -0500

    work on blocks

---
+++ bbmodels/chess/rook_Converted.java
@@ -0,0 +1,40 @@
+// Made with Blockbench 5.1.1
+// Exported for Minecraft version 1.17 or later with Mojang mappings
+// Paste this class into your mod and generate all required imports
+
+
+public class rook_Converted<T extends Entity> extends EntityModel<T> {
+	// This layer location should be baked with EntityRendererProvider.Context in the entity renderer and passed into this model's constructor
+	public static final ModelLayerLocation LAYER_LOCATION = new ModelLayerLocation(new ResourceLocation("modid", "rook_converted"), "main");
+	private final ModelPart piece;
+
+	public rook_Converted(ModelPart root) {
+		this.piece = root.getChild("piece");
+	}
+
+	public static LayerDefinition createBodyLayer() {
+		MeshDefinition meshdefinition = new MeshDefinition();
+		PartDefinition partdefinition = meshdefinition.getRoot();
+
+		PartDefinition piece = partdefinition.addOrReplaceChild("piece", CubeListBuilder.create().texOffs(0, 15).addBox(-2.0F, 7.0F, -2.0F, 4.0F, 1.0F, 4.0F, new CubeDeformation(0.0F))
+		.texOffs(0, 6).mirror().addBox(-1.0F, 3.0F, -1.0F, 2.0F, 2.0F, 2.0F, new CubeDeformation(0.0F)).mirror(false)
+		.texOffs(0, 10).mirror().addBox(-1.5F, 5.0F, -1.5F, 3.0F, 2.0F, 3.0F, new CubeDeformation(0.0F)).mirror(false)
+		.texOffs(0, 0).mirror().addBox(-1.5F, 1.0F, -1.5F, 3.0F, 2.0F, 3.0F, new CubeDeformation(0.0F)).mirror(false)
+		.texOffs(17, 0).addBox(-1.5F, 0.0F, -1.5F, 3.0F, 1.0F, 0.0F, new CubeDeformation(0.0F))
+		.texOffs(17, 0).addBox(-1.5F, 0.0F, 1.5F, 3.0F, 1.0F, 0.0F, new CubeDeformation(0.0F))
+		.texOffs(17, -3).addBox(1.5F, 0.0F, -1.5F, 0.0F, 1.0F, 3.0F, new CubeDeformation(0.0F))
+		.texOffs(17, -3).addBox(-1.5F, 0.0F, -1.5F, 0.0F, 1.0F, 3.0F, new CubeDeformation(0.0F)), PartPose.offset(0.0F, 16.0F, 0.0F));
+
+		return LayerDefinition.create(meshdefinition, 32, 32);
+	}
+
+	@Override
+	public void setupAnim(Entity entity, float limbSwing, float limbSwingAmount, float ageInTicks, float netHeadYaw, float headPitch) {
+
+	}
+
+	@Override
+	public void renderToBuffer(PoseStack poseStack, VertexConsumer vertexConsumer, int packedLight, int packedOverlay, float red, float green, float blue, float alpha) {
+		piece.render(poseStack, vertexConsumer, packedLight, packedOverlay, red, green, blue, alpha);
+	}
+}
