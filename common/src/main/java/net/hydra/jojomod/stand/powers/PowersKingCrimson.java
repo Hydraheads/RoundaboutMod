f290752c3a174777e36e7946ebec452c20f17a42
Hydraheads <Hydraheads42@gmail.com>
2026-07-14 13:25:00 -0500

    kc nitpicks + manga skin

--- common/src/main/java/net/hydra/jojomod/stand/powers/PowersKingCrimson.java
+++ common/src/main/java/net/hydra/jojomod/stand/powers/PowersKingCrimson.java
@@ -1,15 +1,24 @@
 package net.hydra.jojomod.stand.powers;
 
+import com.google.common.collect.Lists;
+import net.hydra.jojomod.access.IPlayerEntity;
 import net.hydra.jojomod.client.ClientNetworking;
 import net.hydra.jojomod.entity.ModEntities;
+import net.hydra.jojomod.entity.stand.KingCrimsonEntity;
 import net.hydra.jojomod.entity.stand.StandEntity;
+import net.hydra.jojomod.entity.stand.StarPlatinumEntity;
 import net.hydra.jojomod.event.index.*;
 import net.hydra.jojomod.event.powers.StandPowers;
 import net.hydra.jojomod.event.powers.StandUser;
+import net.hydra.jojomod.item.MaxStandDiscItem;
 import net.hydra.jojomod.stand.powers.presets.NewPunchingStand;
 import net.minecraft.ChatFormatting;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
+
+import java.util.List;
 
 public class PowersKingCrimson extends NewPunchingStand {
 
@@ -38,6 +47,31 @@ public class PowersKingCrimson extends NewPunchingStand {
     public StandEntity getNewStandEntity() {
         byte sk = ((StandUser) this.getSelf()).roundabout$getStandSkin();
         return ModEntities.KING_CRIMSON.create(this.getSelf().level());
+    }
+    @Override
+    public Component getSkinName(byte skinId) {
+        return getSkinNameT(skinId);
+    }
+    public static Component getSkinNameT(byte skinId){
+        if (skinId == KingCrimsonEntity.MANGA_SKIN){
+            return Component.translatable(  "skins.roundabout.king_crimson.manga");
+        }
+        return Component.translatable(  "skins.roundabout.king_crimson.base");
+    }
+
+    @Override
+    public List<Byte> getSkinList() {
+        List<Byte> $$1 = Lists.newArrayList();
+        $$1.add(KingCrimsonEntity.PART_5_SKIN);
+        if (this.getSelf() instanceof Player PE) {
+            byte Level = ((IPlayerEntity) PE).roundabout$getStandLevel();
+            ItemStack goldDisc = ((StandUser) PE).roundabout$getStandDisc();
+            boolean bypass = PE.isCreative() || (!goldDisc.isEmpty() && goldDisc.getItem() instanceof MaxStandDiscItem);
+            if (Level > 1 || bypass) {
+                $$1.add(KingCrimsonEntity.MANGA_SKIN);
+            }
+        }
+        return $$1;
     }
 
     @Override
