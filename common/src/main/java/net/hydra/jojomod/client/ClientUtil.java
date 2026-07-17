afd4aa8fb4ec189eb7575ae10faa3637bf9248a3
Hydraheads <Hydraheads42@gmail.com>
2026-07-16 23:52:22 -0500

    epitaph screen effect config

--- common/src/main/java/net/hydra/jojomod/client/ClientUtil.java
+++ common/src/main/java/net/hydra/jojomod/client/ClientUtil.java
@@ -908,7 +908,7 @@ public class ClientUtil {
         LocalPlayer player = Minecraft.getInstance().player;
         if (player != null && ((StandUser) player).roundabout$getStandPowers() instanceof PowersKingCrimson PKC) {
             if (PKC.hasStandActive(player)) {
-                return true;
+                return false;
             } else {
                 return false;
             }
