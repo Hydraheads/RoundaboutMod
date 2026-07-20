e9e88d22d7c58728b726c4c74f50e30edad65a53
Feu_Ghost <feughosttrue@gmail.com>
2026-07-21 01:36:34 +0200

    Purple Haze Commit Indeed²

--- common/src/main/java/net/hydra/jojomod/stand/powers/PowersPurpleHaze.java
+++ common/src/main/java/net/hydra/jojomod/stand/powers/PowersPurpleHaze.java
@@ -109,7 +109,6 @@ public class PowersPurpleHaze extends NewPunchingStand {
         return super.tryPower(move, forced);
     }
 
-
     @Override
     public StandEntity getNewStandEntity () {
         return ModEntities.PURPLE_HAZE.create(this.getSelf().level());
