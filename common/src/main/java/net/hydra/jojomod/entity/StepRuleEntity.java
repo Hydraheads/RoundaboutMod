ce4f065427ac63b280e9a80abc4b6aa698eb0bb7
Hydraheads <Hydraheads42@gmail.com>
2026-07-26 19:22:10 -0500

    minor sun damage consistency change

--- common/src/main/java/net/hydra/jojomod/entity/StepRuleEntity.java
+++ common/src/main/java/net/hydra/jojomod/entity/StepRuleEntity.java
@@ -139,7 +139,7 @@ public class StepRuleEntity extends Entity {
                 }
 
                 if (getTurnedBad() && isAlive() && !isRemoved()) {
-                    AABB wallBox = this.getBoundingBox();
+                    AABB wallBox = this.getBoundingBox().inflate(0.1F);
 
                     for (Entity mob : level().getEntitiesOfClass(
                             Entity.class,
