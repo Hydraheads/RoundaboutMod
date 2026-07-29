4ecc6cf60073ccec321e08fe02c6f45ef291ac1a
Hydraheads <Hydraheads42@gmail.com>
2026-07-29 07:53:48 -0500

    knife feedback

--- common/src/main/java/net/hydra/jojomod/entity/projectile/KnifeEntity.java
+++ common/src/main/java/net/hydra/jojomod/entity/projectile/KnifeEntity.java
@@ -7,6 +7,7 @@ import net.hydra.jojomod.client.ClientNetworking;
 import net.hydra.jojomod.entity.BlockWallEntity;
 import net.hydra.jojomod.entity.ModEntities;
 import net.hydra.jojomod.entity.stand.ManhattanTransferEntity;
+import net.hydra.jojomod.entity.visages.CloneEntity;
 import net.hydra.jojomod.event.ModEffects;
 import net.hydra.jojomod.event.powers.ModDamageTypes;
 import net.hydra.jojomod.event.powers.StandUser;
@@ -191,8 +192,8 @@ public class KnifeEntity extends AbstractArrow {
         }
         float $$2;
 
-        if ($$1 instanceof Player) {
-            $$2 = (float) (2.29F * (ClientNetworking.getAppropriateConfig().itemSettings.knifeDamageOnPlayers *0.01));
+        if ($$1 instanceof Player || $$1 instanceof CloneEntity) {
+            $$2 = (float) (2.1F * (ClientNetworking.getAppropriateConfig().itemSettings.knifeDamageOnPlayers *0.01));
 
             if(isHattanKnife){
                 this.doBonusKnifeDamageHattan($$1);
