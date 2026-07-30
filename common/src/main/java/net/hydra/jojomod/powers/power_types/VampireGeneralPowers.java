7670d183d65e36ead38072565e8cae9e41a5351b
Hydraheads <Hydraheads42@gmail.com>
2026-07-29 20:49:34 -0500

    sanity check

--- common/src/main/java/net/hydra/jojomod/powers/power_types/VampireGeneralPowers.java
+++ common/src/main/java/net/hydra/jojomod/powers/power_types/VampireGeneralPowers.java
@@ -15,6 +15,7 @@ import net.hydra.jojomod.event.index.*;
 import net.hydra.jojomod.event.powers.DamageHandler;
 import net.hydra.jojomod.event.powers.ModDamageTypes;
 import net.hydra.jojomod.event.powers.StandUser;
+import net.hydra.jojomod.event.powers.TimeStop;
 import net.hydra.jojomod.fates.powers.VampireFate;
 import net.hydra.jojomod.fates.powers.VampiricFate;
 import net.hydra.jojomod.powers.GeneralPowers;
@@ -673,18 +674,20 @@ public class VampireGeneralPowers extends PunchingGeneralPowers {
                             }
 
                             if (target.hurtTime == 0) {
-                                if (DamageHandler.RipperEyesDamage(target, pow, this.self) && !alreadyBeamed.contains(target)) {
-                                    addToCombo(target);
-                                    bleedEnt(target);
-                                } else if (target.isBlocking()) {
-                                    MainUtil.knockShieldPlusStand(target, 200);
+                                if (!((TimeStop) self.level()).CanTimeStopEntity(self)) {
                                     if (DamageHandler.RipperEyesDamage(target, pow, this.self) && !alreadyBeamed.contains(target)) {
                                         addToCombo(target);
                                         bleedEnt(target);
+                                    } else if (target.isBlocking()) {
+                                        MainUtil.knockShieldPlusStand(target, 200);
+                                        if (DamageHandler.RipperEyesDamage(target, pow, this.self) && !alreadyBeamed.contains(target)) {
+                                            addToCombo(target);
+                                            bleedEnt(target);
+                                        }
                                     }
-                                }
-                                if (!alreadyBeamed.contains(target)) {
-                                    alreadyBeamed.add(target);
+                                    if (!alreadyBeamed.contains(target)) {
+                                        alreadyBeamed.add(target);
+                                    }
                                 }
                             }
 
