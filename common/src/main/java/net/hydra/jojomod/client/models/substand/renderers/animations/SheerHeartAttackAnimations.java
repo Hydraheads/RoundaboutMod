7d8475b555c6fb653daf76de692444f1d9d5c513
Misael <ribeiromisa05@gmail.com>
2026-08-10 21:42:26 -0300

    :coffee: vaccum with a stick

--- common/src/main/java/net/hydra/jojomod/client/models/substand/renderers/animations/SheerHeartAttackAnimations.java
+++ common/src/main/java/net/hydra/jojomod/client/models/substand/renderers/animations/SheerHeartAttackAnimations.java
@@ -10,11 +10,17 @@ public class SheerHeartAttackAnimations {
 
             .build();
 
-    public static final AnimationDefinition MOVING = AnimationDefinition.Builder.withLength(0.2083F).looping()
+    public static final AnimationDefinition MOVING = AnimationDefinition.Builder.withLength(0.2F).looping()
             .addAnimation("stand", new AnimationChannel(AnimationChannel.Targets.POSITION,
                     new Keyframe(0.0F, KeyframeAnimations.posVec(0.0F, 0.2F, 0.0F), AnimationChannel.Interpolations.LINEAR),
-                    new Keyframe(0.0833F, KeyframeAnimations.posVec(0.0F, -0.3F, 0.0F), AnimationChannel.Interpolations.LINEAR),
-                    new Keyframe(0.2083F, KeyframeAnimations.posVec(0.0F, 0.2F, 0.0F), AnimationChannel.Interpolations.LINEAR)
+                    new Keyframe(0.1f, KeyframeAnimations.posVec(0.0F, -0.3F, 0.0F), AnimationChannel.Interpolations.LINEAR),
+                    new Keyframe(0.15f, KeyframeAnimations.posVec(0.0F, 0.2F, 0.0F), AnimationChannel.Interpolations.LINEAR)
+            ))
+            .build();
+
+    public static final AnimationDefinition hideTorch = AnimationDefinition.Builder.withLength(0.0F)
+            .addAnimation("torch", new AnimationChannel(AnimationChannel.Targets.SCALE,
+                    new Keyframe(0.0F, KeyframeAnimations.scaleVec(0.0F, 0.0F, 0.0F), AnimationChannel.Interpolations.LINEAR)
             ))
             .build();
 }
