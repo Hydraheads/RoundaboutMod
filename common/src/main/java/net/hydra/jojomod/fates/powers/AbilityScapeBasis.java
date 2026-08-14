401f99fbc27b3419caa1d0f0b60c7b58af1a17d6
Hydraheads <Hydraheads42@gmail.com>
2026-08-14 18:51:01 -0500

    harpoon nerfs

--- common/src/main/java/net/hydra/jojomod/fates/powers/AbilityScapeBasis.java
+++ common/src/main/java/net/hydra/jojomod/fates/powers/AbilityScapeBasis.java
@@ -124,6 +124,14 @@ public class AbilityScapeBasis {
     }
 
 
+    /**Stand Damage Dropoff**/
+//    public int sdd = 0;
+//    public int sddTime = 0;
+//    public void addSdd(){
+//        sdd++;
+//    }public void maintainSdd(){
+//        sddTime = 20;
+//    }
 
     /**An easy way to replace the EXP bar with a stand bar, see the function below this one*/
     public boolean replaceHudActively(){
