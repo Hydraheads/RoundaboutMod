dd846e92e4d403f2fd0729b596d7a7ca9b844be3
Hydraheads <Hydraheads42@gmail.com>
2026-08-16 16:35:17 -0500

    stand not finished text now IN BOLD + no "complete" levelup text

--- common/src/main/java/net/hydra/jojomod/item/MaxStandDiscItem.java
+++ common/src/main/java/net/hydra/jojomod/item/MaxStandDiscItem.java
@@ -34,9 +34,9 @@ public class MaxStandDiscItem extends StandDiscItem{
     public void appendHoverText(ItemStack $$0, @Nullable Level $$1, List<Component> $$2, TooltipFlag $$3) {
         $$2.add(this.getDisplayName2().withStyle(ChatFormatting.AQUA));
         if(this.standPowers != null && this.standPowers.isWip()){
-            $$2.add(Component.translatable("leveling.roundabout.disc_wip").withStyle(ChatFormatting.RED));
-            $$2.add(Component.translatable("leveling.roundabout.disc_wip_2").withStyle(ChatFormatting.RED));
-            $$2.add(Component.translatable("leveling.roundabout.disc_wip_3").withStyle(ChatFormatting.RED));
+            $$2.add(Component.translatable("leveling.roundabout.disc_wip").withStyle(ChatFormatting.RED).withStyle(ChatFormatting.BOLD));
+            $$2.add(Component.translatable("leveling.roundabout.disc_wip_2").withStyle(ChatFormatting.RED).withStyle(ChatFormatting.BOLD));
+            $$2.add(Component.translatable("leveling.roundabout.disc_wip_3").withStyle(ChatFormatting.RED).withStyle(ChatFormatting.BOLD));
             $$2.add(Component.translatable("roundabout.dev_status.dev_status").withStyle(ChatFormatting.WHITE)
                     .append(" ")
                     .append(this.standPowers.ifWipListDevStatus()));
