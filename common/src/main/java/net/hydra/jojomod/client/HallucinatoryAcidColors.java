ff57a3ac0cbe6501f0c25cc10b61856cf4374b8c
OlivePrincess69 <obliviousthenoob@gmail.com>
2026-08-29 13:18:58 -0400

    whitesnake development and skins

--- common/src/main/java/net/hydra/jojomod/client/HallucinatoryAcidColors.java
+++ common/src/main/java/net/hydra/jojomod/client/HallucinatoryAcidColors.java
@@ -3,6 +3,7 @@ package net.hydra.jojomod.client;
 import net.hydra.jojomod.block.HallucinatoryAcidBlock;
 import net.hydra.jojomod.block.HallucinatoryAcidWallBlock;
 import net.hydra.jojomod.entity.projectile.HallucinatoryAcidProjectile;
+import net.hydra.jojomod.entity.stand.WhitesnakeEntity;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.block.state.BlockState;
 
@@ -12,7 +13,7 @@ public final class HallucinatoryAcidColors {
             0xB0B2ED, 0xDAD9EB, 0xC5A5F7, 0xA9F3AA, 0xECEBC8,
             0xB9F7FB, 0xA386AE, 0xB48082, 0xFFE0B6, 0xDADADA,
             0xDFC7ED, 0xDAD9EB, 0x62858D, 0x676793, 0xCFEBCD,
-            0xB0B2ED, 0xB0B2ED
+            0xB0B2ED, 0xB0B2ED, 0xB0B2ED
     };
     private static final int ANIME_COLOR = SKIN_COLORS[0];
 
@@ -22,7 +23,8 @@ public final class HallucinatoryAcidColors {
     public static int blockColor(BlockState state, int tintIndex) {
         if (tintIndex != 0) return 0xFFFFFF;
         if (state.hasProperty(HallucinatoryAcidBlock.SKIN)) {
-            return tint(state.getValue(HallucinatoryAcidBlock.SKIN));
+            int skin = state.getValue(HallucinatoryAcidBlock.SKIN);
+            return skin == WhitesnakeEntity.SANDSNAKE_SKIN ? 0xFFFFFF : tint(skin);
         }
         if (state.hasProperty(HallucinatoryAcidWallBlock.SKIN)) {
             return tint(state.getValue(HallucinatoryAcidWallBlock.SKIN));
