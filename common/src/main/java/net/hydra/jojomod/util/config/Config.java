591826e3104668d5227f9b9bc2414e1f05f7daa8
Hydraheads <Hydraheads42@gmail.com>
2026-08-30 08:10:21 -0500

    time stop range tweak

--- common/src/main/java/net/hydra/jojomod/util/config/Config.java
+++ common/src/main/java/net/hydra/jojomod/util/config/Config.java
@@ -1432,7 +1432,7 @@ public class Config implements Cloneable {
         public Integer playerDamageCapHealthPercent;
         @BooleanOption(group = "inherit", value = true)
         public Boolean creativeModeInfiniteTimeStop;
-        @IntOption(group = "inherit", value = 100, min = -1, max = 72000)
+        @IntOption(group = "inherit", value = 70, min = -1, max = 72000)
         public Integer blockRangeNegativeOneIsInfinite;
         @BooleanOption(group = "inherit", value = true)
         public Boolean usePreciseMath;
