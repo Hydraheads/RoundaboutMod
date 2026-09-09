b09a8e02c2b81be47f26953375e3a404b93a45be
Hydraheads <Hydraheads42@gmail.com>
2026-09-08 23:35:54 -0500

    early parallel chest work

--- forge/src/main/java/net/hydra/jojomod/registry/ForgeEvents.java
+++ forge/src/main/java/net/hydra/jojomod/registry/ForgeEvents.java
@@ -831,6 +831,7 @@ public class ForgeEvents {
         ModEntities.ROUNDABOUT_BULLET_ENTITY = ForgeEntities.ROUNDABOUT_BULLET_ENTITY.get();
         ModEntities.THROWN_KNIFE = ForgeEntities.THROWN_KNIFE.get();
         ModEntities.BLOCK_WALL = ForgeEntities.BLOCK_WALL.get();
+        ModEntities.PARALLEL_CHEST = ForgeEntities.PARALLEL_CHEST.get();
         ModEntities.D4C_BLOCK = ForgeEntities.D4C_BLOCK.get();
         ModEntities.FALLING_BANNER = ForgeEntities.FALLING_BANNER.get();
         ModEntities.STRAY_CAT = ForgeEntities.STRAY_CAT.get();
